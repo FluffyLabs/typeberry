4c984555725d9060c1b07227e38f53669b412d25
Tomasz Drwięga <tomusdrw@users.noreply.github.com>
2024-07-10 10:53:40 +0200

    Set up simple CI (#4)
    * Create node.js.yml
    
    * Add CI rule.
    
    * Re-format code.
    
    * Fix lint.
    
    * Fix import sorting.
    
    * Fix CI workflow

--- bin/test-runner/json-parser.test.ts
+++ bin/test-runner/json-parser.test.ts
@@ -1,48 +1,53 @@
-import { test } from "node:test";
 import assert from "node:assert";
-import {FromJson, parseFromJson} from "./json-parser";
-
+import { test } from "node:test";
+import { type FromJson, parseFromJson } from "./json-parser";
 
 test("JSON parser", async (t) => {
-	await t.test('parse simple class', () => {
+	await t.test("parse simple class", () => {
 		const json = `{"k": 5, "v": true }`;
 		class TestClass {
 			static fromJson: FromJson<TestClass> = {
-				k: 'number',
-				v: 'boolean',
+				k: "number",
+				v: "boolean",
 			};
 
-			k: number = 0;
-			v: boolean = false;
+			k = 0;
+			v = false;
 		}
 
-		const result = parseFromJson<TestClass>(JSON.parse(json), TestClass.fromJson);
+		const result = parseFromJson<TestClass>(
+			JSON.parse(json),
+			TestClass.fromJson,
+		);
 		assert.strictEqual(result.k, 5);
 		assert.strictEqual(result.v, true);
 	});
 
-	await t.test('parse nested object', () => {
+	await t.test("parse nested object", () => {
 		const json = `{"k": "xyz", "nested": { "k": 5, "v": true }}`;
 		class NestedClass {
 			static fromJson: FromJson<NestedClass> = {
-				k: 'number',
-				v: 'boolean',
+				k: "number",
+				v: "boolean",
 			};
-			k: number = 0;
-			v: boolean = false;
+			k = 0;
+			v = false;
 		}
 
 		class TestClass {
 			static fromJson: FromJson<TestClass> = {
-				k: 'string',
+				k: "string",
 				nested: NestedClass.fromJson,
 			};
 
-			k: string = "";
-			nested: NestedClass = new NestedClass;
+			k = "";
+			nested: NestedClass = new NestedClass();
 		}
 
-		const result = parseFromJson<TestClass>(JSON.parse(json), TestClass.fromJson);
+		const result = parseFromJson<TestClass>(
+			JSON.parse(json),
+			TestClass.fromJson,
+		);
 		assert.strictEqual(result.k, "xyz");
 		assert.deepEqual(result.nested, {
 			k: 5,
@@ -50,19 +55,22 @@ test("JSON parser", async (t) => {
 		});
 	});
 
-	await t.test('parse & process', () => {
+	await t.test("parse & process", () => {
 		const json = `{"k": "0x123", "v": true }`;
 		class TestClass {
 			static fromJson: FromJson<TestClass> = {
-				k: ['string', (v: string) => parseInt(v)],
-				v: 'boolean',
+				k: ["string", (v: string) => Number.parseInt(v)],
+				v: "boolean",
 			};
 
-			k: number = 0;
-			v: boolean = false;
+			k = 0;
+			v = false;
 		}
 
-		const result = parseFromJson<TestClass>(JSON.parse(json), TestClass.fromJson);
+		const result = parseFromJson<TestClass>(
+			JSON.parse(json),
+			TestClass.fromJson,
+		);
 		assert.strictEqual(result.k, 0x123);
 		assert.strictEqual(result.v, true);
 	});
--- bin/test-runner/json-parser.ts
+++ bin/test-runner/json-parser.ts
@@ -1,17 +1,19 @@
-export type FromJson<T> = 
-	FromJsonParser<T, string> |
-	FromJsonParser<T, number> |
-	FromJsonPrimitive<T>;
+export type FromJson<T> =
+	| FromJsonParser<T, string>
+	| FromJsonParser<T, number>
+	| FromJsonPrimitive<T>;
 
-export type FromJsonPrimitive<T> = 
-	T extends string ? 'string' :
-	T extends number ? 'number' :
-	T extends boolean ? 'boolean':
-	T extends object ? ObjectFromJson<T> :
-	never;
+export type FromJsonPrimitive<T> = T extends string
+	? "string"
+	: T extends number
+		? "number"
+		: T extends boolean
+			? "boolean"
+			: T extends object
+				? ObjectFromJson<T>
+				: never;
 
-export type FromJsonParser<T, Y> = 
-	[FromJsonPrimitive<Y>, (inJson: Y) => T];
+export type FromJsonParser<T, Y> = [FromJsonPrimitive<Y>, (inJson: Y) => T];
 
 export type ObjectFromJson<T> = {
 	[K in keyof T]: FromJson<T[K]>;
@@ -19,23 +21,23 @@ export type ObjectFromJson<T> = {
 
 export function parseFromJson<T>(jsonType: unknown, ctor: FromJson<T>): T {
 	const t = typeof jsonType;
-	
-	if (ctor === 'string') {
-		if (t === 'string') {
+
+	if (ctor === "string") {
+		if (t === "string") {
 			return jsonType as T;
 		}
 		throw new Error(`Expected ${ctor} but got ${t}`);
 	}
 
-	if (ctor === 'number') {
-		if (t === 'number') {
+	if (ctor === "number") {
+		if (t === "number") {
 			return jsonType as T;
 		}
 		throw new Error(`Expected ${ctor} but got ${t}`);
 	}
 
-	if (ctor === 'boolean') {
-		if (t === 'boolean') {
+	if (ctor === "boolean") {
+		if (t === "boolean") {
 			return jsonType as T;
 		}
 		throw new Error(`Expected ${ctor} but got ${t}`);
@@ -43,13 +45,13 @@ export function parseFromJson<T>(jsonType: unknown, ctor: FromJson<T>): T {
 
 	if (Array.isArray(ctor)) {
 		const type = ctor[0];
-		if (type === 'string') {
+		if (type === "string") {
 			const parser = ctor[1];
 			const value = parseFromJson<string>(jsonType, type);
 			return parser(value);
 		}
 
-		if (type === 'number') {
+		if (type === "number") {
 			const parser = ctor[1];
 			const value = parseFromJson<number>(jsonType, type);
 			return parser(value);
@@ -57,21 +59,21 @@ export function parseFromJson<T>(jsonType: unknown, ctor: FromJson<T>): T {
 
 		throw new Error(`Invalid parser type: ${type}`);
 	}
-	
-	if (t !== 'object') {
+
+	if (t !== "object") {
 		throw new Error(`Expected complex type but got ${t}`);
 	}
 
-	if (typeof ctor !== 'object') {
+	if (typeof ctor !== "object") {
 		throw new Error(`Unhandled type ${ctor}`);
 	}
 
-	const obj = jsonType as any;
-	const c = ctor as any;
+	const obj = jsonType as { [key: string]: unknown };
+	const c = ctor as { [key: string]: FromJson<unknown> };
 	for (const key of Object.keys(ctor)) {
 		const v = obj[key];
 		obj[key] = parseFromJson(v, c[key]);
 	}
 
-	return obj;
+	return obj as T;
 }
