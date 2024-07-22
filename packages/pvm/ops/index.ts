13851ab12835fe3c257a238abcd2eccc4ad8aa12
Mateusz Sikora <ms1qaz@gmail.com>
2024-07-23 00:31:36 +0200

    pvm: handle move ops

--- packages/pvm/ops/index.ts
+++ packages/pvm/ops/index.ts
@@ -2,3 +2,4 @@ export { BitOps } from "./bit-ops";
 export { ShiftOps } from "./shift-ops";
 export { MathOps } from "./math-ops";
 export { BooleanOps } from "./boolean-ops";
+export { MoveOps } from "./move-ops";
