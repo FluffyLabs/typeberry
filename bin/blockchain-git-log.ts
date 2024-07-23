f6c3eb1e1f314507cea707bfbe3c7cae01746237
Sebastian Koszuta <sebastian.koszuta@gmail.com>
2024-07-23 08:23:53 +0200

    devops: a github workflow uploading hashes of commits pushed to main to a public blockchain

---
+++ bin/blockchain-git-log.ts
@@ -0,0 +1,136 @@
+import * as fs from "node:fs/promises";
+import * as path from "node:path";
+import * as github from "@actions/github";
+import { ApiPromise, WsProvider, Keyring } from "@polkadot/api";
+import type { PushEvent } from "@octokit/webhooks-types";
+
+type TransactionPayload = [
+  string, // repo name
+  string, // ref/branch name
+  number, // timestamp
+  string[], // commit ids
+  string | null // previous block hash
+];
+
+interface LogEntry {
+  payload: TransactionPayload;
+  block: string;
+  status: string;
+  failed: boolean;
+}
+
+const LOG_FILENAME = process.env.LOG_FILENAME as string;
+const AUTH =
+  "deliver resource region two win idea volcano nominee shove phrase horse illness";
+// @todo any ideas on how to store this in a safer manner?
+
+function getPendingCommitIds(
+  log: LogEntry[],
+  eventPayload: PushEvent
+): string[] {
+  const commitIds = [...eventPayload.commits.map((commit) => commit.id)];
+
+  for (let i = log.length - 1; i >= 0; i--) {
+    if (log[i].failed === true) {
+      commitIds.unshift(...log[i].payload[3]);
+    } else {
+      break;
+    }
+  }
+
+  return commitIds;
+}
+
+async function writeLog(log: LogEntry[]) {
+  try {
+    await fs.writeFile(LOG_FILENAME, JSON.stringify(log));
+    console.log("New log written.");
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+async function handleError(
+  log: LogEntry[],
+  transactionPayload: TransactionPayload,
+  error: string
+) {
+  log.push({
+    payload: transactionPayload,
+    status: error,
+    block: "",
+    failed: true,
+  });
+
+  await writeLog(log);
+
+  console.error(error);
+  process.exit(1);
+}
+
+async function main() {
+  const log: LogEntry[] = [];
+
+  try {
+    log.push(...require(path.relative(__dirname, LOG_FILENAME)));
+    console.log("Previous log found. Appending.");
+  } catch (e) {
+    console.log("Previous log not found. Starting a new one.");
+  }
+
+  const eventPayload = github.context.payload as PushEvent;
+  const transactionPayload: TransactionPayload = [
+    eventPayload.repository.name,
+    github.context.ref,
+    Date.now(),
+    getPendingCommitIds(log, eventPayload),
+    log.filter((logEntry) => !logEntry.failed).pop()?.block || "",
+  ];
+
+  const wsProvider = new WsProvider("wss://kusama-asset-hub-rpc.polkadot.io");
+  const api = await ApiPromise.create({ provider: wsProvider });
+
+  const keyring = new Keyring({ type: "sr25519" });
+  const pair = keyring.addFromUri(AUTH);
+
+  const remark = api.tx.system.remark(JSON.stringify(transactionPayload));
+
+  try {
+    console.log("Submitting...");
+
+    const unsub = await remark.signAndSend(
+      pair,
+      async ({ status, dispatchError }) => {
+        console.log(`Transaction status: ${status.type}`);
+
+        if (status.isInBlock) {
+          console.log(`Success. Block hash: ${status.asInBlock.toString()}`);
+
+          log.push({
+            payload: transactionPayload,
+            status: status.type,
+            block: status.asInBlock.toString(),
+            failed: false,
+          });
+
+          await writeLog(log);
+
+          unsub();
+          await api.disconnect();
+        } else if (dispatchError) {
+          await handleError(log, transactionPayload, dispatchError.toString());
+
+          unsub();
+          await api.disconnect();
+        }
+      }
+    );
+  } catch (error) {
+    if (error instanceof Error) {
+      await handleError(log, transactionPayload, error.toString());
+    }
+    await api.disconnect();
+  }
+}
+
+main();
