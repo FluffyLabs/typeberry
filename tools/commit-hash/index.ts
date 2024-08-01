0f391eef312a31d446ae9e378b8ea45d01d8c90d
Sebastian <sebastian.koszuta@gmail.com>
2024-08-01 14:10:58 +0200

    devops: a github workflow uploading hashes of commits pushed to main to a public blockchain (#41)
    * devops: a github workflow uploading hashes of commits pushed to main to a public blockchain
    
    * Move to ./tools; remove secret.
    
    * Format.
    
    * Avoid running tests from node_modules
    
    * Run a test workflow.
    
    * Revert "Run a test workflow."
    
    This reverts commit 48762d609e4c350686fc6c6934a3d0aa16183db2.
    
    * fix types.
    
    * Run the test again.
    
    * Pass COMMIT_KEY_SECRET
    
    * Run only on main.
    
    ---------
    
    Co-authored-by: Tomasz Drwięga <tomusdrw@gmail.com>

---
+++ tools/commit-hash/index.ts
@@ -0,0 +1,129 @@
+import * as fs from "node:fs/promises";
+import * as path from "node:path";
+import * as github from "@actions/github";
+import type { PushEvent } from "@octokit/webhooks-types";
+import { ApiPromise, Keyring, WsProvider } from "@polkadot/api";
+
+type TransactionPayload = [
+  string, // repo name
+  string, // ref/branch name
+  number, // timestamp
+  string[], // commit ids
+  string | null, // previous block hash
+];
+const COMMIT_IDS_INDEX = 3;
+
+interface LogEntry {
+  payload: TransactionPayload;
+  block: string;
+  status: string;
+  failed: boolean;
+}
+
+const LOG_FILENAME = process.env.LOG_FILENAME as string;
+const AUTH = process.env.COMMIT_KEY_SECRET as string;
+
+if (!LOG_FILENAME || !AUTH) {
+  throw new Error("Missing LOG_FILENAME or COMMIT_KEY_SECRET env variables");
+}
+
+function getPendingCommitIds(log: LogEntry[], eventPayload: PushEvent): string[] {
+  const commitIds = [...eventPayload.commits.map((commit) => commit.id)];
+
+  for (let i = log.length - 1; i >= 0; i--) {
+    if (!log[i].failed) {
+      break;
+    }
+    commitIds.unshift(...log[i].payload[COMMIT_IDS_INDEX]);
+  }
+
+  return commitIds;
+}
+
+async function writeLog(log: LogEntry[]) {
+  try {
+    await fs.writeFile(LOG_FILENAME, JSON.stringify(log, null, 2));
+    console.log("New log written.");
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+async function handleError(log: LogEntry[], transactionPayload: TransactionPayload, error: string) {
+  log.push({
+    payload: transactionPayload,
+    status: error,
+    block: "",
+    failed: true,
+  });
+
+  await writeLog(log);
+
+  console.error(error);
+  process.exit(1);
+}
+
+async function main() {
+  const log: LogEntry[] = [];
+
+  try {
+    log.push(...require(path.relative(__dirname, LOG_FILENAME)));
+    console.log("Previous log found. Appending.");
+  } catch (e) {
+    console.log("Previous log not found. Starting a new one.");
+  }
+
+  const eventPayload = github.context.payload as PushEvent;
+  const previousBlockHash = log.filter((logEntry) => !logEntry.failed).pop()?.block || "";
+  const transactionPayload: TransactionPayload = [
+    eventPayload.repository.name,
+    github.context.ref,
+    Date.now(),
+    getPendingCommitIds(log, eventPayload),
+    previousBlockHash,
+  ];
+
+  const wsProvider = new WsProvider("wss://kusama-asset-hub-rpc.polkadot.io");
+  const api = await ApiPromise.create({ provider: wsProvider });
+
+  const keyring = new Keyring({ type: "sr25519" });
+  const pair = keyring.addFromUri(AUTH);
+
+  const remark = api.tx.system.remark(JSON.stringify(transactionPayload));
+
+  try {
+    console.log("Submitting...");
+
+    const unsub = await remark.signAndSend(pair, async ({ status, dispatchError }) => {
+      console.log(`Transaction status: ${status.type}`);
+
+      if (status.isInBlock) {
+        console.log(`Success. Block hash: ${status.asInBlock.toString()}`);
+
+        log.push({
+          payload: transactionPayload,
+          status: status.type,
+          block: status.asInBlock.toString(),
+          failed: false,
+        });
+
+        await writeLog(log);
+
+        unsub();
+        await api.disconnect();
+      } else if (dispatchError) {
+        await handleError(log, transactionPayload, dispatchError.toString());
+
+        unsub();
+        await api.disconnect();
+      }
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      await handleError(log, transactionPayload, error.toString());
+    }
+    await api.disconnect();
+  }
+}
+
+main();
