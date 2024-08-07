4cac819d0f21d7b0f8aa7c96e46403c6840c5475
Mateusz Sikora <ms1qaz@gmail.com>
2024-08-07 11:40:46 +0200

    PVM: rewrite part of tests to use describe-it syntax (#73)
    * PVM: rewrite part of tests to use describe-it syntax
    
    * update tests description
    
    * update tests description part 2

--- packages/pvm/instruction-gas-map.test.ts
+++ packages/pvm/instruction-gas-map.test.ts
@@ -1,15 +1,15 @@
 import assert from "node:assert";
-import { test } from "node:test";
+import { describe, it } from "node:test";
 import { Instruction } from "./instruction";
 import { instructionGasMap } from "./instruction-gas-map";
 
-test("instructionGasMap", async (t) => {
+describe("instructionGasMap", () => {
   const instructions = Object.entries(Instruction).filter(
     (entry): entry is [string, number] => typeof entry[0] === "string" && typeof entry[1] === "number",
   );
 
   for (const [name, instruction] of instructions) {
-    await t.test(`checks if instruction ${name} = ${instruction} is correctly mapped to gas value`, () => {
+    it(`checks if instruction ${name} = ${instruction} is correctly mapped to gas value`, () => {
       const gasValue = instructionGasMap[instruction];
       assert.notEqual(null, gasValue);
     });
