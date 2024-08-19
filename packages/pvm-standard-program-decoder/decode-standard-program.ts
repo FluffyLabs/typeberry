7cf42123cac5ac6ebff371ee17f2608b66e66e10
Mateusz Sikora <ms1qaz@gmail.com>
2024-08-19 11:56:04 +0200

    add standard program decoder package

---
+++ packages/pvm-standard-program-decoder/decode-standard-program.ts
@@ -0,0 +1,90 @@
+import { LittleEndianDecoder } from "@typeberry/jam-codec/little-endian-decoder";
+import { checkAndType, Opaque } from "@typeberry/utils";
+
+import { ARGS_SEGMENT, DATA_LEGNTH, LAST_PAGE, PAGE_SIZE, SEGMENT_SIZE, STACK_SEGMENT } from "./memory-conts";
+import { increaseToPageSize, increaseToSegmentSize } from "./memory-utils";
+
+const decoder = new LittleEndianDecoder();
+
+/**
+ * program = E_3(|o|) ++ E_3(|w|) ++ E_2(z) ++ E_3(s) ++ o ++ w ++ E_4(|c|) ++ c
+ * 
+ * E_n - little endian encoding, n - length
+ * o - initial read only data
+ * w - initial heap
+ * z - heap pages filled with zeros
+ * s - stack size
+ * c - program code
+ */
+
+const READONLY_LENGTH_INDEX = 0;
+const HEAP_LENGTH_INDEX = READONLY_LENGTH_INDEX + 3;
+const NO_OF_HEAP_PAGES_INDEX = HEAP_LENGTH_INDEX + 3;
+const STACK_SIZE_INDEX = NO_OF_HEAP_PAGES_INDEX + 2;
+const READONLY_DATA_INDEX = STACK_SIZE_INDEX + 3;
+
+type InputLength = Opaque<number, 'Number that is lower than 2 ** 24 (Z_I from GP)'>;
+
+export function decodeStandardProgram(program: Uint8Array, args: Uint8Array) {
+    const oLength = decoder.decodeU32(program.subarray(READONLY_LENGTH_INDEX, HEAP_LENGTH_INDEX));
+    const wLength = decoder.decodeU32(program.subarray(HEAP_LENGTH_INDEX, NO_OF_HEAP_PAGES_INDEX));
+    const argsLength = checkAndType<number, InputLength>(args.length, args.length <= DATA_LEGNTH, 'Incorrect arguments length');
+    const readOnlyLength =checkAndType<number, InputLength>(oLength, oLength <= DATA_LEGNTH, 'Incorrect readonly segment length');
+    const heapLength = checkAndType<number, InputLength>(wLength, wLength <= DATA_LEGNTH, 'Incorrect heap segment length');
+    const noOfHeapZerosPages = decoder.decodeU32(program.subarray(NO_OF_HEAP_PAGES_INDEX, STACK_SIZE_INDEX));
+    const stackSize = decoder.decodeU32(program.subarray(STACK_SIZE_INDEX, READONLY_DATA_INDEX));
+    const heapIndex = READONLY_DATA_INDEX + readOnlyLength;
+    const readOnlyMemory = program.subarray(READONLY_DATA_INDEX, heapIndex);
+    const codeLengthIndex = heapIndex + heapLength;
+    const initialHeap = program.subarray(heapIndex, codeLengthIndex);
+    const codeIndex = codeLengthIndex + 4;
+    const codeLength = decoder.decodeU32(program.subarray(codeLengthIndex, codeIndex));
+    const code = program.subarray(codeIndex, codeIndex + codeLength);
+
+    const readonlyDataStart = SEGMENT_SIZE;
+    const readonlyDataEnd = SEGMENT_SIZE + readOnlyLength;
+    const readOnlyZerosEnd = SEGMENT_SIZE + increaseToPageSize(readOnlyLength);
+    const heapDataStart = 2 * SEGMENT_SIZE + increaseToSegmentSize(readOnlyLength);
+    const heapDataEnd = heapDataStart + heapLength;
+    const heapZerosEnd = heapDataStart + increaseToPageSize(heapLength) + noOfHeapZerosPages * PAGE_SIZE;
+    const stackStart = STACK_SEGMENT - increaseToPageSize(stackSize);
+    const stackEnd = STACK_SEGMENT;
+    const argsStart = ARGS_SEGMENT
+    const argsEnd = argsStart + argsLength;
+    const argsZerosEnd = argsEnd + increaseToPageSize(argsLength);
+    
+    return {
+        code,
+        memory: {
+            readable: [
+                getMemorySegment(readonlyDataStart, readonlyDataEnd, readOnlyMemory),
+                getMemorySegment(readonlyDataEnd, readOnlyZerosEnd),
+                getMemorySegment(argsStart, argsEnd, args),
+                getMemorySegment(argsEnd, argsZerosEnd),
+            ],
+            writeable: [
+                getMemorySegment(heapDataStart, heapDataEnd, initialHeap),
+                getMemorySegment(heapDataEnd, heapZerosEnd),
+                getMemorySegment(stackStart, stackEnd),
+            ],
+
+            sbrkIndex: heapZerosEnd
+        },
+        registers: getRegisters(args.length),
+    }
+}
+
+function getMemorySegment(start: number, end: number, data: Uint8Array|null = null) {
+    return {start, end, data};
+}
+
+function getRegisters(argsLength: number) {
+    const regs = new Uint8Array(13);
+    
+    regs[1] = LAST_PAGE;
+    regs[2] = STACK_SEGMENT;
+    regs[10] = ARGS_SEGMENT;
+    regs[11] = argsLength;
+
+    return regs;
+}
--- packages/utils/debug.ts
+++ packages/utils/debug.ts
@@ -10,3 +10,24 @@ export function check(condition: boolean, message?: string): asserts condition i
     throw new Error(`Assertion failure: ${message || ""}`);
   }
 }
+
+function cast<T, U extends T>(a: T, condition: boolean): a is U {
+  return condition;
+}
+
+/**
+ * Yet another function to perform runtime assertions. 
+ * This function returns a new type to mark in the code that this value was checked and you don't have to do it again.
+ * 
+ * In the post-processing step all usages of this functions should be replaced with simple casting. An example:
+ * const x = checkAndType<number, CheckedNumber>(y);
+ * should be replaced with:
+ * const x = y as CheckedNumber;
+ */
+export function checkAndType<T, U extends T>(a: T, condition: boolean, message?: string): U {
+  if (cast<T, U>(a, condition)) {
+    return a;
+  } 
+
+  throw new Error(`Assertion failure: ${message || ""}`);
+}
