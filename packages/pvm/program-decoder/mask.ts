802cddcaa4790e0bdfc18bafcd8c0104879139e5
Mateusz Sikora <ms1qaz@gmail.com>
2024-08-21 08:40:51 +0200

    fix problems with basic block validation and parsing immediate that has length equal to 0 (#88)

--- packages/pvm/program-decoder/mask.ts
+++ packages/pvm/program-decoder/mask.ts
@@ -1,3 +1,4 @@
+import { check } from "@typeberry/utils";
 export class Mask {
   /**
    * The lookup table will have `0` at the index which corresponds to an instruction on the same index in the bytecode.
@@ -6,29 +7,36 @@ export class Mask {
    * Example:
    * ```
    * 0..1..2..3..4..5..6..7..8..9 # Indices
-   * 0..2..1..0..1..0..3..2..1..0 # lookupTable values
+   * 0..2..1..0..1..0..3..2..1..0 # lookupTable forward values
+   * 0..1..2..0..1..0..1..2..3..0 # lookupTable backward values
    * ```
    * There are instructions at indices `0, 3, 5, 9`.
    */
-  private lookupTable: Uint8Array;
+  private lookupTableForward: Uint8Array;
+  private lookupTableBackward: Uint8Array;
 
   constructor(mask: Uint8Array) {
-    this.lookupTable = this.buildLookupTable(mask);
+    this.lookupTableForward = this.buildLookupTableForward(mask);
+    this.lookupTableBackward = this.buildLookupTableBackward(mask);
   }
 
   isInstruction(index: number) {
-    return this.lookupTable[index] === 0;
+    return this.lookupTableForward[index] === 0;
   }
 
   getNoOfBytesToNextInstruction(index: number) {
-    if (this.isInstruction(index)) {
-      const nextIndex = Math.min(index + 1, this.lookupTable.length - 1);
-      return this.lookupTable[nextIndex] + 1;
-    }
-    return this.lookupTable[index];
+    check(index >= 0, "index cannot be a negative number");
+    check(index < this.lookupTableForward.length, `index cannot be bigger than ${this.lookupTableForward.length}`);
+    return this.lookupTableForward[index];
   }
 
-  private buildLookupTable(mask: Uint8Array) {
+  getNoOfBytesToPreviousInstruction(index: number) {
+    check(index >= 0, "index cannot be a negative number");
+    check(index < this.lookupTableBackward.length, `index cannot be bigger than ${this.lookupTableBackward.length}`);
+    return this.lookupTableBackward[index];
+  }
+
+  private buildLookupTableForward(mask: Uint8Array) {
     const table = new Uint8Array(mask.length * 8);
     let lastInstructionOffset = 0;
     for (let i = mask.length - 1; i >= 0; i--) {
@@ -41,6 +49,24 @@ export class Mask {
         }
         table[i * 8 + j] = lastInstructionOffset;
         singleBitMask >>>= 1;
+      }
+    }
+    return table;
+  }
+
+  private buildLookupTableBackward(mask: Uint8Array) {
+    const table = new Uint8Array(mask.length * 8);
+    let lastInstructionOffset = 0;
+    for (let i = 0; i < mask.length; i++) {
+      let singleBitMask = 0x01;
+      for (let j = 0; j < 8; j++) {
+        if ((mask[i] & singleBitMask) > 0) {
+          lastInstructionOffset = 0;
+        } else {
+          lastInstructionOffset++;
+        }
+        table[i * 8 + j] = lastInstructionOffset;
+        singleBitMask <<= 1;
       }
     }
     return table;
