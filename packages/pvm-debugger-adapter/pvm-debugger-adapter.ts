70fa814c3bb598437d2ea6cbc97a0c1d823ba09b
Mateusz Sikora <ms1qaz@gmail.com>
2024-09-11 11:15:23 +0200

    PVM: add adapter package that adjust PVM interface to debugger tool (#103)
    * add adapter package that adjust PVM interface to debugger tool
    
    * add comments

---
+++ packages/pvm-debugger-adapter/pvm-debugger-adapter.ts
@@ -0,0 +1,49 @@
+import { Pvm } from "@typeberry/pvm";
+import { PAGE_SIZE } from "@typeberry/pvm/memory/memory-consts";
+
+export class PvmDebuggerAdapter {
+  private pvm: Pvm;
+
+  constructor(...args: ConstructorParameters<typeof Pvm>) {
+    this.pvm = new Pvm(...args);
+  }
+  
+  nextStep() {
+    return this.pvm.nextStep();
+  }
+
+  getRegisters() {
+    return this.pvm.getRegisters();
+  }
+
+  getPC() {
+    return this.pvm.getPC();
+  }
+
+  getGas() {
+    return this.pvm.getGas();
+  }
+
+  getStatus() {
+    return this.pvm.getStatus();
+  }
+
+  getMemoryPage(pageNumber: number): null | Uint8Array {
+    const page = this.pvm.getMemoryPage(pageNumber);
+
+    if (!page) {
+      // page wasn't allocated so we return an empty page
+      return new Uint8Array(PAGE_SIZE);
+    }
+
+    if (page.length === PAGE_SIZE) {
+      // page was allocated and has a proper size so we can simply return it
+      return page;
+    }
+
+    // page was allocated but it is shorter than PAGE_SIZE so we have to extend it
+    const fullPage = new Uint8Array(PAGE_SIZE);
+    fullPage.set(page);
+    return fullPage;
+  }
+}
