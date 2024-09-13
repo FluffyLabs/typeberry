ab5e2256710f7d954f636b3fa3e240425cce167a
Mateusz Sikora <ms1qaz@gmail.com>
2024-09-13 15:28:59 +0200

    rename const and add comment

--- packages/pvm/memory/pages/page-utils.ts
+++ packages/pvm/memory/pages/page-utils.ts
@@ -1,6 +1,6 @@
 import { type Opaque, ensure } from "@typeberry/utils";
 
-import { LAST_PAGE_NUMBER, PAGE_SIZE } from "../memory-consts";
+import { LAST_PAGE_START_MEMORY_INDEX, PAGE_SIZE } from "../memory-consts";
 
 export type PageIndex = Opaque<number, "memory page index">;
 export type PageNumber = Opaque<number, "memory page number">;
@@ -14,13 +14,22 @@ export function createPageIndex(index: number) {
 export function createPageNumber(index: number) {
   return ensure<number, PageNumber>(
     index,
-    index >= 0 && index * PAGE_SIZE <= LAST_PAGE_NUMBER,
+    index >= 0 && index * PAGE_SIZE <= LAST_PAGE_START_MEMORY_INDEX,
     `Incorrect page number: ${index}!`,
   );
 }
 
-/** Get the next page number and wrap the result if it is bigger than LAST_PAGE_NUMBER */
+/**
+ * Get the next page number and wrap the result if it is bigger than LAST_PAGE_NUMBER
+ *
+ * GP references:
+ * 1. The modulo subscription operator is used in all load/store instructions, for example:
+ * https://graypaper.fluffylabs.dev/#WyI3YWU1MWY5MzI1IiwiMjMiLCJBY2tub3dsZWRnZW1lbnRzIixudWxsLFsiPGRpdiBjbGFzcz1cInQgbTAgeDllIGg2IHk3NjcgZmY3IGZzMCBmYzAgc2MwIGxzMCB3czBcIj4iLCI8ZGl2IGNsYXNzPVwidCBtMCB4OWUgaDYgeTc2NyBmZjcgZnMwIGZjMCBzYzAgbHMwIHdzMFwiPiJdXQ==
+ *
+ * 2. Here is the definition of the modulo subscription operator:
+ * https://graypaper.fluffylabs.dev/#WyI3YWU1MWY5MzI1IiwiNyIsIk5vdGF0aW9uYWwgQ29udmVudGlvbnMiLCJDcnlwdG9ncmFwaHkiLFsiPGRpdiBjbGFzcz1cInQgbTAgeDEyIGhiIHk0YyBmZjcgZnMwIGZjMCBzYzAgbHMwIHdzMFwiPiIsIjxkaXYgY2xhc3M9XCJ0IG0wIHgzOCBoYiB5NGMgZmYxMyBmczAgZmMwIHNjMCBsczAgd3MwXCI+Il1d
+ */
 export function getNextPageNumber(pageNumber: PageNumber): PageNumber {
-  const newPageNumber = pageNumber === LAST_PAGE_NUMBER ? 0 : pageNumber + 1;
+  const newPageNumber = pageNumber === LAST_PAGE_START_MEMORY_INDEX ? 0 : pageNumber + 1;
   return createPageNumber(newPageNumber);
 }
