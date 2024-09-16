3151e3213521ff7babceb83699dce9b83a21a8ce
Mateusz Sikora <ms1qaz@gmail.com>
2024-09-16 13:30:31 +0200

    cleaning

--- packages/pvm/memory/pages/memory-page.ts
+++ packages/pvm/memory/pages/memory-page.ts
@@ -5,12 +5,10 @@ import { getStartPageIndexFromPageNumber } from "../memory-utils";
 import type { PageIndex, PageNumber } from "./page-utils";
 
 export abstract class MemoryPage {
-  // public end: MemoryIndex;
   public start: MemoryIndex;
 
   constructor(pageNumber: PageNumber) {
     this.start = getStartPageIndexFromPageNumber(pageNumber);
-    // this.end = createMemoryIndex(this.start + PAGE_SIZE);
   }
 
   abstract loadInto(res: Uint8Array, address: PageIndex, length: number): null | PageFault;
