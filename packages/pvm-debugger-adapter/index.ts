a07ff1d3f37c75a34c747c32fe572d2df8fc4ec0
Mateusz Sikora <ms1qaz@gmail.com>
2024-10-11 12:14:21 +0200

    export more utils to use in debugger

--- packages/pvm-debugger-adapter/index.ts
+++ packages/pvm-debugger-adapter/index.ts
@@ -3,3 +3,5 @@ export { PvmDebuggerAdapter as Pvm } from "./pvm-debugger-adapter";
 export { ProgramDecoder } from "@typeberry/pvm/program-decoder/program-decoder";
 export { ArgsDecoder } from "@typeberry/pvm/args-decoder/args-decoder";
 export { ArgumentType } from "@typeberry/pvm/args-decoder/argument-type";
+export { createResults } from "@typeberry/pvm/args-decoder/args-decoding-results";
+export { instructionArgumentTypeMap } from "@typeberry/pvm/args-decoder/instruction-argument-type-map";
