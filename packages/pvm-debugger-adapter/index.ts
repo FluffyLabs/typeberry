4eef738d5aec538336acf9196f755618aaf324be
Mateusz Sikora <ms1qaz@gmail.com>
2024-10-11 10:08:32 +0200

    export utils to use in PVM debugger

--- packages/pvm-debugger-adapter/index.ts
+++ packages/pvm-debugger-adapter/index.ts
@@ -1,2 +1,5 @@
 export { Memory, MemoryBuilder } from "@typeberry/pvm";
 export { PvmDebuggerAdapter as Pvm } from "./pvm-debugger-adapter";
+export { ProgramDecoder } from "@typeberry/pvm/program-decoder/program-decoder";
+export { ArgsDecoder } from "@typeberry/pvm/args-decoder/args-decoder";
+export { ArgumentType } from "@typeberry/pvm/args-decoder/argument-type";
