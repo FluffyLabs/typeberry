db9fbaab7c2df39c022dc94d2dcc8e64883a6ca8
Tomasz Drwięga <tomusdrw@gmail.com>
2024-10-17 08:26:16 +0200

    Serve the index.html file directly from the RPC server.

--- tools/ipc2rpc/rpc.ts
+++ tools/ipc2rpc/rpc.ts
@@ -1,4 +1,6 @@
+import fs from "node:fs";
 import http from "node:http";
+import path from "node:path";
 
 import type { Header } from "@typeberry/block";
 import { Bytes, BytesBlob } from "@typeberry/bytes";
@@ -15,43 +17,44 @@ export function startRpc(db: Database) {
   });
 
   const httpServer = http.createServer((req, res) => {
-    if (req.method === "POST") {
-      let body = "";
-      // Collect the data
-      req.on("data", (chunk) => {
-        body += chunk.toString();
-      });
-
-      // When all data has been received
-      req.on("end", () => {
-        server.receiveJSON(body).then((response) => {
-          if (response) {
-            res.writeHead(200, {
-              "content-type": "application/json",
-              "access-control-allow-origin": "*",
-            });
-            res.write(
-              JSON.stringify(response, (key, val) => {
-                if (val instanceof Bytes) {
-                  return val.toString();
-                }
-                if (val instanceof BytesBlob) {
-                  return val.toString();
-                }
-                return val;
-              }),
-            );
-            res.end();
-          } else {
-            res.writeHead(204);
-            res.end();
-          }
-        });
-      });
-    } else {
-      res.writeHead(405);
-      res.end("use POST");
+    if (req.method !== "POST") {
+      res.writeHead(200);
+      fs.createReadStream(path.join(__dirname, "index.html")).pipe(res);
+      return;
     }
+
+    let body = "";
+    // Collect the data
+    req.on("data", (chunk) => {
+      body += chunk.toString();
+    });
+
+    // When all data has been received
+    req.on("end", () => {
+      server.receiveJSON(body).then((response) => {
+        if (response) {
+          res.writeHead(200, {
+            "content-type": "application/json",
+            "access-control-allow-origin": "*",
+          });
+          res.write(
+            JSON.stringify(response, (key, val) => {
+              if (val instanceof Bytes) {
+                return val.toString();
+              }
+              if (val instanceof BytesBlob) {
+                return val.toString();
+              }
+              return val;
+            }),
+          );
+          res.end();
+        } else {
+          res.writeHead(204);
+          res.end();
+        }
+      });
+    });
   });
 
   httpServer.listen(3000, () => {
