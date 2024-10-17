1e4ac20aabab2de3a4413dd83e1e7f3633de2372
Tomasz Drwięga <tomusdrw@gmail.com>
2024-10-17 10:39:42 +0200

    Work Packages.

--- packages/transition/index.ts
+++ packages/transition/index.ts
@@ -1,7 +1,10 @@
 import { Extrinsic, type ExtrinsicHash, Header, type HeaderHash, WithHash } from "@typeberry/block";
 import type { ChainSpec } from "@typeberry/block/context";
+import {WorkPackage} from "@typeberry/block/work-package";
+import {WorkReport} from "@typeberry/block/work-report";
 import { Encoder } from "@typeberry/codec";
 import { type HashAllocator, hashBytes } from "@typeberry/hash";
+import {InMemoryBlocks} from "../database";
 
 export class TransitionHasher {
   constructor(
@@ -19,5 +22,19 @@ export class TransitionHasher {
     // TODO [ToDr] Use already allocated encoding destination and hash bytes from some arena.
     const encoded = Encoder.encodeObject(Extrinsic.Codec, extrinsic, this.context);
     return new WithHash(hashBytes(encoded, this.allocator) as ExtrinsicHash, extrinsic);
+  }
+}
+
+export class WorkPackageExecutor {
+
+  constructor(
+    public readonly database: InMemoryBlocks;
+  ) {}
+
+  // TODO [ToDr] this while thing should be triple-checked with the GP.
+  // I'm currently implementing some dirty version for the demo.
+  executeWorkPackage(package: WorkPackage): WorkReport {
+    // execute authorisation first or is it already executed and we just need to check it?
+    // then validate & execute each work item and generate the report.
   }
 }
