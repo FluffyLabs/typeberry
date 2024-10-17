fbbc8f34c5a086c87a9e259bc9a239f0a769ce1a
Tomasz Drwięga <tomusdrw@gmail.com>
2024-10-17 15:42:50 +0200

    Publish block package.

--- packages/numbers/index.ts
+++ packages/numbers/index.ts
@@ -1,4 +1,7 @@
-declare const __REPRESENTATION_BYTES__: unique symbol;
+// TODO [ToDr] This should be `unique symbol`, but for some reason
+// I can't figure out how to build `@typeberry/blocks` package.
+declare const __REPRESENTATION_BYTES__: "REPRESENTATION_BYTES";
+
 type WithBytesRepresentation<Bytes extends number> = {
   readonly [__REPRESENTATION_BYTES__]: Bytes;
 };
