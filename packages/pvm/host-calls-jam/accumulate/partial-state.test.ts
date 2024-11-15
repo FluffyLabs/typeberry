9dd716ffc13e3b4ebcd77873395cc13710481e3c
Tomasz Drwięga <tomusdrw@gmail.com>
2024-11-15 22:08:22 +0100

    Transfer host call.

--- packages/pvm/host-calls-jam/accumulate/partial-state.test.ts
+++ packages/pvm/host-calls-jam/accumulate/partial-state.test.ts
@@ -1,15 +1,35 @@
 import type { CoreIndex, ServiceId } from "@typeberry/block";
+import type { Bytes } from "@typeberry/bytes";
 import type { FixedSizeArray, KnownSizeArray } from "@typeberry/collections";
 import type { Blake2bHash } from "@typeberry/hash";
+import type { U64 } from "@typeberry/numbers";
 import type { Gas } from "@typeberry/pvm-interpreter/gas";
 import type { ValidatorData } from "@typeberry/safrole";
-import type { AUTHORIZATION_QUEUE_SIZE, AccumulationPartialState } from "./partial-state";
+import { Result } from "@typeberry/utils";
+import type {
+  AUTHORIZATION_QUEUE_SIZE,
+  AccumulationPartialState,
+  TRANSFER_MEMO_BYTES,
+  TransferError,
+} from "./partial-state";
 
 export class TestAccumulate implements AccumulationPartialState {
   public readonly privilegedServices: Parameters<TestAccumulate["updatePrivilegedServices"]>[] = [];
   public readonly authQueue: Parameters<TestAccumulate["updateAuthorizationQueue"]>[] = [];
   public readonly validatorsData: Parameters<TestAccumulate["updateValidatorsData"]>[0][] = [];
+  public readonly transferData: Parameters<TestAccumulate["transfer"]>[] = [];
   public checkpointCalled = 0;
+  public transferReturnValue: Result<null, TransferError> = Result.ok(null);
+
+  transfer(
+    destination: ServiceId,
+    amount: U64,
+    suppliedGas: Gas,
+    memo: Bytes<TRANSFER_MEMO_BYTES>,
+  ): Result<null, TransferError> {
+    this.transferData.push([destination, amount, suppliedGas, memo]);
+    return this.transferReturnValue;
+  }
 
   checkpoint(): void {
     this.checkpointCalled += 1;
--- packages/pvm/host-calls-jam/accumulate/partial-state.ts
+++ packages/pvm/host-calls-jam/accumulate/partial-state.ts
@@ -1,13 +1,39 @@
 import type { CoreIndex, ServiceId } from "@typeberry/block";
-import { Q } from "@typeberry/block/gp-constants";
+import { Q, W_T } from "@typeberry/block/gp-constants";
+import type { Bytes } from "@typeberry/bytes";
 import type { FixedSizeArray, KnownSizeArray } from "@typeberry/collections";
 import type { Blake2bHash } from "@typeberry/hash";
+import type { U64 } from "@typeberry/numbers";
 import type { Gas } from "@typeberry/pvm-interpreter/gas";
 import type { ValidatorData } from "@typeberry/safrole";
+import type { Result } from "@typeberry/utils";
 
 /** Size of the authorization queue. */
 export const AUTHORIZATION_QUEUE_SIZE = Q;
 export type AUTHORIZATION_QUEUE_SIZE = typeof AUTHORIZATION_QUEUE_SIZE;
+
+/** Size of the transfer memo. */
+export const TRANSFER_MEMO_BYTES = W_T;
+export type TRANSFER_MEMO_BYTES = typeof TRANSFER_MEMO_BYTES;
+
+/**
+ * Errors that may occur when the transfer is invoked.
+ *
+ * TODO [ToDr] Since I don't fully understand yet which of these
+ * could be checked directly in the host call (i.e. if we will
+ * have access to the service account state there) for now I keep
+ * them safely in the `AccumulationPartialState` implementation.
+ * However, if possible, these should be moved directly to the
+ * host call implementation.
+ */
+export enum TransferError {
+  /** The destination service does not exist. */
+  DestinationNotFound = 0,
+  /** The supplied gas is too low to execute `OnTransfer` entry point. */
+  GasTooLow = 1,
+  /** After transfering the funds account balance would be below the threshold. */
+  BalanceBelowThreshold = 2,
+}
 
 /**
  * `U`: state components mutated by the accumulation.
@@ -19,6 +45,18 @@ export type AUTHORIZATION_QUEUE_SIZE = typeof AUTHORIZATION_QUEUE_SIZE;
  * https://graypaper.fluffylabs.dev/#/439ca37/161402161402
  */
 export interface AccumulationPartialState {
+  /**
+   * Transfer given `amount` of funds to the `destination`,
+   * passing `suppliedGas` to invoke `OnTransfer` entry point
+   * and given `memo`.
+   */
+  transfer(
+    destination: ServiceId,
+    amount: U64,
+    suppliedGas: Gas,
+    memo: Bytes<TRANSFER_MEMO_BYTES>,
+  ): Result<null, TransferError>;
+
   /** Designate new validators given their key and meta data. */
   updateValidatorsData(validatorsData: KnownSizeArray<ValidatorData, "ValidatorsCount">): void;
 
