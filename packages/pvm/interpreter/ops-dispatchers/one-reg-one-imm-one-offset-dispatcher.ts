231185751e72ffebe93f5f4b3aec92d175ab12a0
Mateusz Sikora <ms1qaz@gmail.com>
2024-12-19 12:51:20 +0100

    fix

--- packages/pvm/interpreter/ops-dispatchers/one-reg-one-imm-one-offset-dispatcher.ts
+++ packages/pvm/interpreter/ops-dispatchers/one-reg-one-imm-one-offset-dispatcher.ts
@@ -3,6 +3,7 @@ import { Instruction } from "../instruction";
 import type { BranchOps, LoadOps } from "../ops";
 import { signExtend32To64 } from "../registers";
 
+const MASK = 0xffff_ffff_ffff_ffffn;
 export class OneRegOneImmOneOffsetDispatcher {
   constructor(
     private branchOps: BranchOps,
@@ -18,66 +19,70 @@ export class OneRegOneImmOneOffsetDispatcher {
       case Instruction.BRANCH_EQ_IMM:
         this.branchOps.branchEqImmediate(
           args.registerIndex,
-          signExtend32To64(args.immediateDecoder.getUnsigned()),
+          signExtend32To64(args.immediateDecoder.getUnsigned()) & MASK,
           args.nextPc,
         );
         break;
       case Instruction.BRANCH_NE_IMM:
-        this.branchOps.branchNeImmediate(args.registerIndex, BigInt(args.immediateDecoder.getUnsigned()), args.nextPc);
+        this.branchOps.branchNeImmediate(
+          args.registerIndex,
+          BigInt(args.immediateDecoder.getUnsigned()) & MASK,
+          args.nextPc,
+        );
         break;
       case Instruction.BRANCH_LT_U_IMM:
         this.branchOps.branchLtUnsignedImmediate(
           args.registerIndex,
-          BigInt(args.immediateDecoder.getUnsigned()),
+          BigInt(args.immediateDecoder.getUnsigned()) & MASK,
           args.nextPc,
         );
         break;
       case Instruction.BRANCH_LE_U_IMM:
         this.branchOps.branchLeUnsignedImmediate(
           args.registerIndex,
-          BigInt(args.immediateDecoder.getUnsigned()),
+          BigInt(args.immediateDecoder.getUnsigned()) & MASK,
           args.nextPc,
         );
         break;
       case Instruction.BRANCH_GE_U_IMM:
         this.branchOps.branchGeUnsignedImmediate(
           args.registerIndex,
-          BigInt(args.immediateDecoder.getUnsigned()),
+          BigInt(args.immediateDecoder.getUnsigned()) & MASK,
           args.nextPc,
         );
         break;
       case Instruction.BRANCH_GT_U_IMM:
         this.branchOps.branchGtUnsignedImmediate(
           args.registerIndex,
-          BigInt(args.immediateDecoder.getUnsigned()),
+          BigInt(args.immediateDecoder.getUnsigned()) & MASK,
           args.nextPc,
         );
         break;
       case Instruction.BRANCH_LT_S_IMM:
         this.branchOps.branchLtSignedImmediate(
           args.registerIndex,
-          BigInt(args.immediateDecoder.getSigned()),
+          signExtend32To64(args.immediateDecoder.getSigned()),
           args.nextPc,
         );
         break;
       case Instruction.BRANCH_LE_S_IMM:
         this.branchOps.branchLeSignedImmediate(
           args.registerIndex,
-          BigInt(args.immediateDecoder.getSigned()),
+          signExtend32To64(args.immediateDecoder.getSigned()),
           args.nextPc,
         );
         break;
       case Instruction.BRANCH_GE_S_IMM:
         this.branchOps.branchGeSignedImmediate(
           args.registerIndex,
-          BigInt(args.immediateDecoder.getSigned()),
+          signExtend32To64(args.immediateDecoder.getSigned()),
           args.nextPc,
         );
         break;
       case Instruction.BRANCH_GT_S_IMM:
         this.branchOps.branchGtSignedImmediate(
           args.registerIndex,
-          BigInt(args.immediateDecoder.getSigned()),
+          signExtend32To64(args.immediateDecoder.getSigned()),
           args.nextPc,
         );
         break;
