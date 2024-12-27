f6e3e5f0b80b6e51af4375a059693336a1ec197d
Mateusz Sikora <ms1qaz@gmail.com>
2024-12-27 12:34:45 +0100

    Update packages/trie/masks.ts
    Co-authored-by: Tomek Drwięga <tomusdrw@users.noreply.github.com>

--- packages/trie/masks.ts
+++ packages/trie/masks.ts
@@ -1,5 +1,5 @@
-export const EMBED_LEAF_NODE_MASK = 0b10_00_00_00;
-export const NAGATED_EMBED_LEAF_NODE_MASK = 0b01_11_11_11;
+export const FIRST_BIT_SET = 0b10_00_00_00;
+export const FIRST_BIT_SET_NEG = 0b01_11_11_11;
 
-export const LEAF_NODE_MASK = 0b11_00_00_00;
-export const NAGATED_LEAF_NODE_MASK = 0b00_11_11_11;
+export const FIRST_TWO_BITS_SET = 0b11_00_00_00;
+export const FIRST_TWO_BITS_SET_NEG = 0b00_11_11_11;
