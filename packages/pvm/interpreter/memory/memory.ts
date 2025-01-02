d7fe27635596e24331ccefbf6588651794162140
Mateusz Sikora <ms1qaz@gmail.com>
2025-01-02 11:17:14 +0100

    Fix memory allocation (#215)
    * Revert "allocate whole page"
    
    This reverts commit dc28f3fe55b4e383529ee0019c7a42c1eb557260.
    
    * do not return PageFault in case of reading/writing on allocated but not requested memory

--- packages/pvm/interpreter/memory/memory.ts
+++ packages/pvm/interpreter/memory/memory.ts
@@ -14,23 +14,31 @@ type InitialMemoryState = {
 
 export class Memory {
   static fromInitialMemory(initialMemoryState: InitialMemoryState) {
-    return new Memory(initialMemoryState?.sbrkIndex, initialMemoryState?.endHeapIndex, initialMemoryState?.memory);
+    return new Memory(
+      initialMemoryState?.sbrkIndex,
+      initialMemoryState?.sbrkIndex,
+      initialMemoryState?.endHeapIndex,
+      initialMemoryState?.memory,
+    );
   }
 
   constructor(
     private sbrkIndex = tryAsSbrkIndex(0),
+    private virtualSbrkIndex = tryAsSbrkIndex(0),
     private endHeapIndex = tryAsSbrkIndex(MAX_MEMORY_INDEX),
     private memory = new Map<PageNumber, MemoryPage>(),
   ) {}
 
   reset() {
     this.sbrkIndex = tryAsSbrkIndex(0);
+    this.virtualSbrkIndex = tryAsSbrkIndex(0);
     this.endHeapIndex = tryAsSbrkIndex(MAX_MEMORY_INDEX);
     this.memory = new Map<PageNumber, MemoryPage>(); // TODO [MaSi]: We should keep allocated pages somewhere and reuse it when it is possible
   }
 
   copyFrom(memory: Memory) {
     this.sbrkIndex = memory.sbrkIndex;
+    this.virtualSbrkIndex = memory.virtualSbrkIndex;
     this.endHeapIndex = memory.endHeapIndex;
     this.memory = memory.memory;
   }
@@ -164,13 +172,21 @@ export class Memory {
     return null;
   }
 
-  sbrk(lengthToAllocate: number): SbrkIndex {
-    const length = alignToPageSize(lengthToAllocate);
+  sbrk(length: number): SbrkIndex {
     const currentSbrkIndex = this.sbrkIndex;
+    const currentVirtualSbrkIndex = this.virtualSbrkIndex;
 
     // new sbrk index is bigger than 2 ** 32 or endHeapIndex
-    if (MAX_MEMORY_INDEX < currentSbrkIndex + length || currentSbrkIndex + length > this.endHeapIndex) {
+    if (MAX_MEMORY_INDEX < currentVirtualSbrkIndex + length || currentVirtualSbrkIndex + length > this.endHeapIndex) {
       throw new OutOfMemory();
+    }
+
+    const newVirtualSbrkIndex = tryAsSbrkIndex(this.virtualSbrkIndex + length);
+
+    // no alllocation needed
+    if (newVirtualSbrkIndex <= currentSbrkIndex) {
+      this.virtualSbrkIndex = newVirtualSbrkIndex;
+      return currentVirtualSbrkIndex;
     }
 
     // standard allocation using "Writeable" pages
@@ -184,6 +200,7 @@ export class Memory {
       this.memory.set(pageNumber, page);
     }
 
+    this.virtualSbrkIndex = tryAsSbrkIndex(currentSbrkIndex + length);
     this.sbrkIndex = newSbrkIndex;
     return currentSbrkIndex;
   }
