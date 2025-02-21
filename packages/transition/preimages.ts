a475e2b68a7406be1d024c6f809b220afa8a6fd9
Sebastian Koszuta <sebastian.koszuta@gmail.com>
2025-02-21 10:17:25 +0100

    Simplified preimage status transitions

--- packages/transition/preimages.ts
+++ packages/transition/preimages.ts
@@ -33,19 +33,6 @@ export enum PreimagesErrorCode {
   AccountNotFound = "account_not_found",
 }
 
-/**
- * Preimage status is determined by the number of timeslots in a lookup history
- * item, where each timeslot describes when the particular status was achieved.
- * Status transitions can only happen one step at a time in the following order:
- * Requested (denoted by empty array) -> Available -> Unavailable -> Reavailable
- * docs: https://graypaper.fluffylabs.dev/#/5f542d7/116f00116f00 */
-export enum PreimageStatus {
-  Requested = 0,
-  Available = 1,
-  Unavailable = 2,
-  Reavailable = 3,
-}
-
 const MAX_LOOKUP_HISTORY_SLOTS = 3;
 export type LookupHistorySlots = KnownSizeArray<TimeSlot, "0-3 timeslots">;
 export function tryAsLookupHistorySlots(items: TimeSlot[]): LookupHistorySlots {
@@ -81,33 +68,8 @@ export class LookupHistoryItem {
     public slots: LookupHistorySlots,
   ) {}
 
-  // https://graypaper.fluffylabs.dev/#/5f542d7/116f0011a500
-  public setAvailable(slot: TimeSlot) {
-    if (this.slots.length > 0) {
-      throw new Error('Illegal transition: To set preimage state to "available", it must be "requested" first.');
-    }
-
-    this.slots = tryAsLookupHistorySlots([slot]);
-  }
-
-  public setUnavailable(slot: TimeSlot) {
-    if (this.slots.length !== 1) {
-      throw new Error('Illegal transition: To set preimage state to "unavailable", it must be "available" first.');
-    }
-
-    this.slots = tryAsLookupHistorySlots([this.slots[0], slot]);
-  }
-
-  public setReavailable(slot: TimeSlot) {
-    if (this.slots.length !== 2) {
-      throw new Error('Illegal transition: To set preimage state to "re-available", it must be "unavailable" first.');
-    }
-
-    this.slots = tryAsLookupHistorySlots([this.slots[0], this.slots[1], slot]);
-  }
-
-  public getStatus(): PreimageStatus {
-    return this.slots.length; // it's only a coincidence that enum values correspond to length but taking advantage of it
+  public isRequested(): boolean {
+    return this.slots.length === 0;
   }
 }
 
@@ -139,7 +101,6 @@ export class Preimages {
       hash: PreimageHash;
       blob: BytesBlob;
       lookupHistoryItem: LookupHistoryItem;
-      slot: TimeSlot;
     }[] = [];
 
     // select preimages for integration
@@ -156,11 +117,7 @@ export class Preimages {
 
       // https://graypaper.fluffylabs.dev/#/5f542d7/181800181900
       // https://graypaper.fluffylabs.dev/#/5f542d7/116f0011a500
-      if (
-        account.data.preimages.has(hash) ||
-        !lookupHistoryItem ||
-        lookupHistoryItem.getStatus() !== PreimageStatus.Requested
-      ) {
+      if (account.data.preimages.has(hash) || !lookupHistoryItem || !lookupHistoryItem.isRequested()) {
         return Result.error(PreimagesErrorCode.PreimageUnneeded);
       }
 
@@ -169,15 +126,14 @@ export class Preimages {
         hash,
         blob,
         lookupHistoryItem,
-        slot,
       });
     }
 
     // https://graypaper.fluffylabs.dev/#/5f542d7/18c00018f300
     for (const change of pendingChanges) {
-      const { account, hash, blob, lookupHistoryItem, slot } = change;
+      const { account, hash, blob, lookupHistoryItem } = change;
       account.data.preimages.set(hash, blob);
-      lookupHistoryItem.setAvailable(slot);
+      lookupHistoryItem.slots = tryAsLookupHistorySlots([slot]);
     }
 
     return Result.ok(null);
