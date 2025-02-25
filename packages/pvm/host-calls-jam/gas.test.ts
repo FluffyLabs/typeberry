15c2790f9a6a400d41e978f21cc1e874e79695a9
r0tc <87579231+r0tc@users.noreply.github.com>
2025-02-25 17:42:25 +0100

    refactor gas host call to 0.6.1 gray paper version (#273)
    * add .idea webstorm/intellij configuration to .gitignore
    
    * replace custom wait with node promise
    
    * refactor gas host call to latest graypaper
    
    * fix checkpoint host call test

--- packages/pvm/host-calls-jam/gas.test.ts
+++ packages/pvm/host-calls-jam/gas.test.ts
@@ -4,41 +4,36 @@ import { gasCounter, tryAsGas } from "@typeberry/pvm-interpreter/gas";
 import { Registers } from "@typeberry/pvm-interpreter/registers";
 import { Gas as GasHostCall } from "./gas";
 
-const REG_LOWER = 7;
-const REG_UPPER = 8;
+const REGISTER = 7;
 
 describe("HostCalls: Gas", () => {
-  it("should write U32 gas to registers", () => {
+  it("should write U32 gas to register", () => {
     const gas = new GasHostCall();
 
     const counter = gasCounter(tryAsGas(10_000));
     const regs = new Registers();
 
-    assert.deepStrictEqual(regs.getU32(REG_LOWER), 0);
-    assert.deepStrictEqual(regs.getU32(REG_UPPER), 0);
+    assert.deepStrictEqual(regs.getU64(REGISTER), 0n);
 
     // when
     gas.execute(counter, regs);
 
     // then
-    assert.deepStrictEqual(regs.getU32(REG_LOWER), 10_000);
-    assert.deepStrictEqual(regs.getU32(REG_UPPER), 0);
+    assert.deepStrictEqual(regs.getU64(REGISTER), 10_000n);
   });
 
-  it("should write U64 gas to registers", () => {
+  it("should write U64 gas to register", () => {
     const gas = new GasHostCall();
 
-    const counter = gasCounter(tryAsGas(2n ** 42n - 1n));
+    const counter = gasCounter(tryAsGas(2n ** 64n - 1n));
     const regs = new Registers();
 
-    assert.deepStrictEqual(regs.getU32(REG_LOWER), 0);
-    assert.deepStrictEqual(regs.getU32(REG_UPPER), 0);
+    assert.deepStrictEqual(regs.getU64(REGISTER), 0n);
 
     // when
     gas.execute(counter, regs);
 
     // then
-    assert.deepStrictEqual(regs.getU32(REG_LOWER), 0xffffffff);
-    assert.deepStrictEqual(regs.getU32(REG_UPPER), 0x3ff);
+    assert.deepStrictEqual(regs.getU64(REGISTER), 2n ** 64n - 1n);
   });
 });
