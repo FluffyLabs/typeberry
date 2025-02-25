a953f7697a0c5b98ea8c6b8d852b3b1c4a0fd228
Mateusz Sikora <ms1qaz@gmail.com>
2025-02-25 15:27:16 +0100

    refactor

--- packages/crypto/ed25519.ts
+++ packages/crypto/ed25519.ts
@@ -1,4 +1,4 @@
-import type { Ed25519Key, Ed25519Signature } from "@typeberry/block";
+import { ED25519_KEY_BYTES, ED25519_SIGNATURE_BYTES, type Ed25519Key, type Ed25519Signature } from "@typeberry/block";
 import { BytesBlob } from "@typeberry/bytes";
 import { verify_ed25519, verify_ed25519_batch } from "ed25519-wasm/pkg";
 
@@ -25,13 +25,25 @@ export async function verify<T extends BytesBlob>(input: Input<T>[]): Promise<bo
   if (input.length === 0) {
     return Promise.resolve([]);
   }
+  const dataLength = input.reduce(
+    (acc, { message, key, signature }) => acc + key.length + signature.length + message.length + 1,
+    0,
+  );
+  const data = new Uint8Array(dataLength);
 
-  const [first, ...rest] = input.map(({ key, signature, message }) => {
-    const messageLength = message.raw.length;
-    return BytesBlob.blobFromParts(key.raw, signature.raw, Uint8Array.from([messageLength]), message.raw).raw;
-  });
+  let offset = 0;
 
-  const data = BytesBlob.blobFromParts(first, ...rest).raw;
+  for (const { key, message, signature } of input) {
+    data.set(key.raw, offset);
+    offset += ED25519_KEY_BYTES;
+    data.set(signature.raw, offset);
+    offset += ED25519_SIGNATURE_BYTES;
+    const messageLength = message.length;
+    data[offset] = messageLength;
+    offset += 1;
+    data.set(message.raw, offset);
+    offset += messageLength;
+  }
 
   const result = Array.from(verify_ed25519(data)).map((x) => x === 1);
   return Promise.resolve(result);
