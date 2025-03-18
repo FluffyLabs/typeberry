db9fc6725ff591b0c7aa008b5c60334cc38b6682
r0tc <87579231+r0tc@users.noreply.github.com>
2025-03-18 22:53:36 +0100

    add eqeqeq rule as error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -18,6 +18,7 @@ export default tseslint.config(
       },
     },
     rules: {
+      eqeqeq: "error",
       "import/no-extraneous-dependencies": "error",
       "@typescript-eslint/strict-boolean-expressions": [
         "error",
