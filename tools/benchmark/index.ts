e7b551829bd67c745ba2abaa16ab124f65957bac
r0tc <87579231+r0tc@users.noreply.github.com>
2025-03-19 20:58:03 +0100

    refactor benchmark errors handling

--- tools/benchmark/index.ts
+++ tools/benchmark/index.ts
@@ -64,13 +64,18 @@ async function runAllBenchmarks() {
     }
   }
 
-  const hasErrors =
-    Array.from(results.entries()).filter(([_key, result]) => {
-      return result.diff.find((e: OkResult | ErrorResult) => "err" in e) !== undefined;
-    }).length > 0;
+  const errorKeys: string[] = [];
+  for (const [key, res] of Array.from(results.entries())) {
+    if (res.diff.find((e: OkResult | ErrorResult) => "err" in e) !== undefined) {
+      errorKeys.push(key);
+    }
+  }
+  const hasErrors = errorKeys.length > 0;
 
   if (hasErrors) {
-    throw new Error("Errors while running benchmarks. Exiting.");
+    const error = new Error(`${errorKeys.length} errors while running benchmarks. Exiting.`);
+    error.cause = errorKeys.join(", ");
+    throw error;
   }
 }
 
