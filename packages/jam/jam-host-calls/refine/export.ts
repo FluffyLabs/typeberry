cf569e9530c863b3818d32401ca675dfeaaf3dac
DrEverr <ms1997sokol@gmail.com>
2025-04-01 14:53:48 +0200

    export update

--- packages/jam/jam-host-calls/refine/export.ts
+++ packages/jam/jam-host-calls/refine/export.ts
@@ -1,6 +1,6 @@
 import { SEGMENT_BYTES, type Segment } from "@typeberry/block";
 import { Bytes } from "@typeberry/bytes";
-import { type HostCallHandler, type PvmExecution, tryAsHostCallIndex } from "@typeberry/pvm-host-calls";
+import { type HostCallHandler, PvmExecution, tryAsHostCallIndex } from "@typeberry/pvm-host-calls";
 import {
   type GasCounter,
   type Memory,
@@ -8,7 +8,7 @@ import {
   tryAsMemoryIndex,
   tryAsSmallGas,
 } from "@typeberry/pvm-interpreter";
-import { LegacyHostCallResult } from "../results";
+import { HostCallResult } from "../results";
 import { CURRENT_SERVICE_ID } from "../utils";
 import type { RefineExternalities } from "./refine-externalities";
 
@@ -20,7 +20,7 @@ const IN_OUT_REG = 7;
  * https://graypaper.fluffylabs.dev/#/579bd12/340b02340b02
  */
 export class Export implements HostCallHandler {
-  index = tryAsHostCallIndex(16);
+  index = tryAsHostCallIndex(19);
   gasCost = tryAsSmallGas(10);
   currentServiceId = CURRENT_SERVICE_ID;
 
@@ -36,8 +36,7 @@ export class Export implements HostCallHandler {
 
     const segmentReadPageFault = memory.loadInto(segment.raw.subarray(0, segmentLength), segmentStart);
     if (segmentReadPageFault !== null) {
-      regs.setU32(IN_OUT_REG, LegacyHostCallResult.OOB);
-      return;
+      return PvmExecution.Panic;
     }
 
     // attempt to export a segment and fail if it's above the maximum.
@@ -45,8 +44,7 @@ export class Export implements HostCallHandler {
     if (segmentExported.isOk) {
       regs.setU32(IN_OUT_REG, segmentExported.ok);
     } else {
-      regs.setU32(IN_OUT_REG, LegacyHostCallResult.FULL);
+      regs.setU64(IN_OUT_REG, HostCallResult.FULL);
     }
-    return;
   }
 }
