64e7db5a8f07fff6daf191db5d1872e9747b352c
r0tc <87579231+r0tc@users.noreply.github.com>
2025-04-06 12:03:12 +0200

    Refactor read hc to GP 0.6.4

--- packages/jam/jam-host-calls/read.ts
+++ packages/jam/jam-host-calls/read.ts
@@ -1,17 +1,18 @@
 import type { ServiceId } from "@typeberry/block";
 import type { BytesBlob } from "@typeberry/bytes";
 import { type Blake2bHash, blake2b } from "@typeberry/hash";
+import { minU64, tryAsU64 } from "@typeberry/numbers";
 import type { HostCallHandler } from "@typeberry/pvm-host-calls";
 import {
   type Memory,
-  type PvmExecution,
+  PvmExecution,
   type Registers,
   tryAsHostCallIndex,
 } from "@typeberry/pvm-host-calls/host-call-handler";
 import { type GasCounter, tryAsSmallGas } from "@typeberry/pvm-interpreter/gas";
 import { tryAsMemoryIndex } from "@typeberry/pvm-interpreter/memory/memory-index";
-import { LegacyHostCallResult } from "./results";
-import { CURRENT_SERVICE_ID, SERVICE_ID_BYTES, legacyGetServiceId, writeServiceIdAsLeBytes } from "./utils";
+import { HostCallResult } from "./results";
+import { CURRENT_SERVICE_ID, SERVICE_ID_BYTES, getServiceId, writeServiceIdAsLeBytes } from "./utils";
 
 /** Account data interface for Read host call. */
 export interface Accounts {
@@ -28,7 +29,7 @@ const IN_OUT_REG = 7;
 /**
  * Read account storage.
  *
- * https://graypaper.fluffylabs.dev/#/579bd12/304101304101
+ * https://graypaper.fluffylabs.dev/#/68eaa1f/302701302701?v=0.6.4
  */
 export class Read implements HostCallHandler {
   index = tryAsHostCallIndex(2);
@@ -39,38 +40,53 @@ export class Read implements HostCallHandler {
 
   async execute(_gas: GasCounter, regs: Registers, memory: Memory): Promise<undefined | PvmExecution> {
     // a
-    const serviceId = legacyGetServiceId(IN_OUT_REG, regs, this.currentServiceId);
+    const serviceId = getServiceId(IN_OUT_REG, regs, this.currentServiceId);
+    if (serviceId === null) {
+      regs.setU64(IN_OUT_REG, HostCallResult.NONE);
+      return;
+    }
+
     // k_0
     const keyStartAddress = tryAsMemoryIndex(regs.getU32(8));
     // k_z
-    const keyLen = regs.getU32(9);
-    // b_0
-    const destinationStart = tryAsMemoryIndex(regs.getU32(10));
-    // b_z
-    const destinationLen = regs.getU32(11);
+    const keyLen = tryAsMemoryIndex(regs.getU32(9));
+    // o
+    const destinationAddress = tryAsMemoryIndex(regs.getU32(10));
 
     // allocate extra bytes for the serviceId
     const key = new Uint8Array(SERVICE_ID_BYTES + keyLen);
     writeServiceIdAsLeBytes(this.currentServiceId, key);
-    const keyLoadingFault = memory.loadInto(key.subarray(SERVICE_ID_BYTES), keyStartAddress);
-    const destinationWriteable = memory.isWriteable(destinationStart, destinationLen);
-
-    // we return OOB in case the destination is not writeable or the key can't be loaded.
-    if (keyLoadingFault !== null || !destinationWriteable) {
-      regs.setU32(IN_OUT_REG, LegacyHostCallResult.OOB);
-      return;
+    const pageFault = memory.loadInto(key.subarray(SERVICE_ID_BYTES), keyStartAddress);
+    if (pageFault !== null) {
+      return Promise.resolve(PvmExecution.Panic);
     }
 
     const keyHash = blake2b.hashBytes(key);
+
+    // v
     const value = await this.account.read(serviceId, keyHash);
 
     if (value === null) {
-      regs.setU32(IN_OUT_REG, LegacyHostCallResult.NONE);
+      regs.setU64(IN_OUT_REG, HostCallResult.NONE);
       return;
     }
 
-    // copy value to the memory and set the length to register 7
-    memory.storeFrom(destinationStart, value.raw.subarray(0, destinationLen));
-    regs.setU32(IN_OUT_REG, value.raw.length);
+    const valueLength = tryAsU64(value.raw.length);
+    const valueBlobOffset = tryAsU64(regs.getU64(11));
+    const lengthToWrite = tryAsU64(regs.getU64(12));
+
+    // f
+    const start = minU64(valueBlobOffset, valueLength);
+    // l
+    const blobLength = minU64(lengthToWrite, tryAsU64(valueLength - start));
+
+    const writePageFault = memory.storeFrom(
+      destinationAddress,
+      value.raw.subarray(Number(start), Number(start + blobLength)),
+    );
+    if (writePageFault !== null) {
+      return Promise.resolve(PvmExecution.Panic);
+    }
+    regs.setU64(IN_OUT_REG, valueLength);
   }
 }
