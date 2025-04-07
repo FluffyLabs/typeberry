d36ffe6d70354064ef6fd6cde8ed4a1e415e8f55
Tomek Drwięga <tomusdrw@users.noreply.github.com>
2025-04-07 07:50:31 +0200

    State merkleization & jamduna test vectors stub. (#302)
    * Merkelize state.
    
    * Rename w3f json test cases to support other suites.
    
    * Refactor to support different suites.
    
    * Load jamduna test vectors and check state roots.
    
    * Update bin/test-runner/jamduna/stateLoader.ts
    
    Co-authored-by: Mateusz Sikora <ms1qaz@gmail.com>
    
    * Update bin/test-runner/jamduna/stateLoader.ts
    
    Co-authored-by: Mateusz Sikora <ms1qaz@gmail.com>
    
    * Update lockfile.
    
    ---------
    
    Co-authored-by: Mateusz Sikora <ms1qaz@gmail.com>

---
+++ bin/test-runner/common.ts
@@ -0,0 +1,138 @@
+import "json-bigint-patch";
+
+import { fail } from "node:assert";
+import * as fs from "node:fs/promises";
+import path from "node:path";
+import test from "node:test";
+import { type FromJson, parseFromJson } from "@typeberry/json-parser";
+import { Level, Logger } from "@typeberry/logger";
+
+Logger.configureAll(process.env.JAM_LOG ?? "", Level.LOG);
+export const logger = Logger.new(__filename, "test-runner");
+
+export function runner<T>(
+  name: string,
+  fromJson: FromJson<T>,
+  run: (test: T, path: string) => Promise<void>,
+): Runner<unknown> {
+  return { name, fromJson, run } as Runner<unknown>;
+}
+
+export type Runner<T> = {
+  name: string;
+  fromJson: FromJson<T>;
+  run: (test: T, path: string) => Promise<void>;
+};
+
+export async function main(runners: Runner<unknown>[], directoryToScan: string, inFiles: string[]) {
+  const relPath = `${__dirname}/../..`;
+  const tests: TestAndRunner[] = [];
+
+  let files = inFiles;
+  if (files.length === 0) {
+    // scan the jamtestvectors directory
+    files = await scanDir(relPath, directoryToScan, ".json");
+  }
+
+  logger.info(`Creating tests for ${files.length} files.`);
+  for (const file of files) {
+    const absolutePath = path.resolve(`${relPath}/${file}`);
+    const data = await fs.readFile(absolutePath, "utf8");
+    const testContent = JSON.parse(data);
+    const testCase = prepareTest(runners, testContent, file, absolutePath);
+
+    tests.push(testCase);
+  }
+
+  // now we're going to aggregate the tests by their runner.
+  const aggregated = new Map<string, TestAndRunner[]>();
+  for (const test of tests) {
+    const sameRunner = aggregated.get(test.runner) ?? [];
+    sameRunner.push(test);
+    aggregated.set(test.runner, sameRunner);
+  }
+
+  const pathToReplace = new RegExp(`/.*${directoryToScan}/`);
+  logger.info(`Running all tests (${tests.length}).`);
+  // we have all of the tests now, let's run them in parallel and generate results.
+  for (const [key, values] of aggregated.entries()) {
+    // split large suites into parts to run them in parallel
+    const perPart = key === "safrole" ? 5 : 50;
+    const parts = Math.ceil(values.length / perPart);
+    for (let i = 0; i < parts; i += 1) {
+      // we use `setImmediate` here, to make sure to start each suite
+      // separately (faster feedback in the console when running tests).
+      setImmediate(() => {
+        const testName = `${key} tests [${i + 1}/${parts}]`;
+        logger.info(`Running ${testName}`);
+        test.describe(
+          testName,
+          {
+            concurrency: 100,
+            timeout: 60 * 1000,
+          },
+          () => {
+            const partValues = values.slice(i * perPart, (i + 1) * perPart);
+            for (const subTest of partValues) {
+              const fileName = subTest.file.replace(pathToReplace, "");
+              test.it(fileName, subTest.test);
+            }
+          },
+        );
+      });
+    }
+  }
+
+  return "Tests registered successfully";
+}
+
+async function scanDir(relPath: string, dir: string, filePattern: string): Promise<string[]> {
+  const files = await fs.readdir(`${relPath}/${dir}`, {
+    recursive: true,
+  });
+  return files.filter((f) => f.endsWith(filePattern)).map((f) => `${dir}/${f}`);
+}
+
+type TestAndRunner = {
+  runner: string;
+  file: string;
+  test: () => Promise<void>;
+};
+
+function prepareTest(runners: Runner<unknown>[], testContent: unknown, file: string, path: string): TestAndRunner {
+  const errors: [string, unknown][] = [];
+  const handleError = (name: string, e: unknown) => errors.push([name, e]);
+
+  // Find the first runner that is able to parse the input data.
+  for (const { name, fromJson, run } of runners) {
+    // the condition is needed to distinguish between tiny and full chain spec
+    // without the condition some tests (for example statistics) will be run twice
+    if (!name.split("/").every((pathPart) => path.includes(pathPart))) {
+      continue;
+    }
+
+    try {
+      const parsedTest = parseFromJson(testContent, fromJson);
+
+      return {
+        runner: name,
+        file,
+        test: () => run(parsedTest, path),
+      };
+    } catch (e) {
+      handleError(name, e);
+    }
+  }
+
+  return {
+    runner: "Invalid",
+    file,
+    test: () => {
+      for (const [runner, error] of errors) {
+        logger.error(`[${runner}] Parsing error: ${error}`);
+      }
+
+      fail(`Unrecognized test case in ${file}`);
+    },
+  };
+}
