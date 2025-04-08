7a2fcc1232f1f04802409a904a40f905f8045872
Tomasz Drwięga <tomusdrw@gmail.com>
2025-04-08 12:05:38 +0200

    Split large verifySeal function.

--- packages/jam/safrole/safrole-seal.ts
+++ packages/jam/safrole/safrole-seal.ts
@@ -1,6 +1,16 @@
-import { type EntropyHash, type HeaderView, encodeUnsealedHeader } from "@typeberry/block";
+import {
+  type BandersnatchKey,
+  type EntropyHash,
+  type HeaderView,
+  type PerEpochBlock,
+  type PerValidator,
+  type TimeSlot,
+  type ValidatorIndex,
+  encodeUnsealedHeader,
+} from "@typeberry/block";
+import type { Ticket } from "@typeberry/block/tickets";
 import { BytesBlob } from "@typeberry/bytes";
-import type { State } from "@typeberry/state";
+import type { State, ValidatorData } from "@typeberry/state";
 import { SafroleSealingKeysKind } from "@typeberry/state/safrole-data";
 import { Result } from "@typeberry/utils";
 import { verifySeal } from "./bandersnatch";
@@ -70,33 +80,72 @@ export class SafroleSeal {
     const entropy = state.currentEntropy;
 
     if (sealingKeys.kind === SafroleSealingKeysKind.Tickets) {
-      const index = timeSlot % sealingKeys.tickets.length;
-      const { id, attempt } = sealingKeys.tickets[index];
-      const payload = BytesBlob.blobFromParts(JAM_TICKET_SEAL, entropy.raw, new Uint8Array([attempt]));
-      // verify seal correctness
-      const result = await verifySeal(
-        await this.bandersnatch,
-        validators.map((x) => x.bandersnatch),
+      return await this.verifySealWithTicket(
+        sealingKeys.tickets,
+        timeSlot,
+        entropy,
+        validators,
         validatorIndex,
-        headerView.seal.materialize(),
-        payload,
-        encodeUnsealedHeader(headerView),
+        headerView,
       );
-
-      if (result.isError) {
-        return Result.error(SafroleSealError.IncorrectSeal);
-      }
-
-      if (!id.isEqualTo(result.ok)) {
-        return Result.error(SafroleSealError.InvalidTicket);
-      }
-
-      return Result.ok(result.ok);
     }
 
-    // Fallback mode
-    const index = timeSlot % sealingKeys.keys.length;
-    const sealingKey = sealingKeys.keys[index];
+    return await this.verifySealWithKeys(
+      sealingKeys.keys,
+      authorKey,
+      timeSlot,
+      entropy,
+      validators,
+      validatorIndex,
+      headerView,
+    );
+  }
+
+  /** Regular (non-fallback) mode of Safrole. */
+  async verifySealWithTicket(
+    tickets: PerEpochBlock<Ticket>,
+    timeSlot: TimeSlot,
+    entropy: EntropyHash,
+    validators: PerValidator<ValidatorData>,
+    validatorIndex: ValidatorIndex,
+    headerView: HeaderView,
+  ): Promise<Result<EntropyHash, SafroleSealError>> {
+    const index = timeSlot % tickets.length;
+    const { id, attempt } = tickets[index];
+    const payload = BytesBlob.blobFromParts(JAM_TICKET_SEAL, entropy.raw, new Uint8Array([attempt]));
+    // verify seal correctness
+    const result = await verifySeal(
+      await this.bandersnatch,
+      validators.map((x) => x.bandersnatch),
+      validatorIndex,
+      headerView.seal.materialize(),
+      payload,
+      encodeUnsealedHeader(headerView),
+    );
+
+    if (result.isError) {
+      return Result.error(SafroleSealError.IncorrectSeal);
+    }
+
+    if (!id.isEqualTo(result.ok)) {
+      return Result.error(SafroleSealError.InvalidTicket);
+    }
+
+    return Result.ok(result.ok);
+  }
+
+  /** Fallback mode of Safrole. */
+  async verifySealWithKeys(
+    keys: PerEpochBlock<BandersnatchKey>,
+    authorKey: ValidatorData,
+    timeSlot: TimeSlot,
+    entropy: EntropyHash,
+    validators: PerValidator<ValidatorData>,
+    validatorIndex: ValidatorIndex,
+    headerView: HeaderView,
+  ): Promise<Result<EntropyHash, SafroleSealError>> {
+    const index = timeSlot % keys.length;
+    const sealingKey = keys[index];
     if (!sealingKey.isEqualTo(authorKey.bandersnatch)) {
       return Result.error(SafroleSealError.InvalidValidator);
     }
