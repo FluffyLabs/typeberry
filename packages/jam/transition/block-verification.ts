85b070ef3f9f1b43a254c8711086dda555ebebd2
Tomasz Drwięga <tomusdrw@gmail.com>
2025-04-10 10:20:16 +0200

    Add gp links to block verification.

--- packages/jam/transition/block-verification.ts
+++ packages/jam/transition/block-verification.ts
@@ -11,10 +11,12 @@ export class BlockVerifier {
 
   async verifyBlock(block: BlockView): Promise<Result<HeaderHash, BlockVerifierError>> {
     // TODO [ToDr] verify according to GP:
-    // - parent exists,
-    // - time slot index consecutive,
-    // - seal valid,
-    // - state root valid, etc
+    // https://github.com/FluffyLabs/typeberry/issues/316#issue-2966031139
+    // - parent exists (https://graypaper.fluffylabs.dev/#/68eaa1f/0c9d000c9d00?v=0.6.4)
+    // - time slot index consecutive (https://graypaper.fluffylabs.dev/#/68eaa1f/0c02010c0201?v=0.6.4)
+    // - seal valid?
+    // - state root valid (https://graypaper.fluffylabs.dev/#/68eaa1f/0c18010c1801?v=0.6.4)
+    // - etc
     const headerView = block.header.view();
     const headerHash = this.hasher.header(headerView);
 
