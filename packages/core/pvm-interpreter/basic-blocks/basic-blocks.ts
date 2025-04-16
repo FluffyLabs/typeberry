7923b9392602ba662817f189d99c1e2c45d30594
Mateusz Sikora <ms1qaz@gmail.com>
2025-04-16 13:37:25 +0200

    review fixes

--- packages/core/pvm-interpreter/basic-blocks/basic-blocks.ts
+++ packages/core/pvm-interpreter/basic-blocks/basic-blocks.ts
@@ -1,35 +1,25 @@
-import { Mask } from "../program-decoder/mask";
+import type { Mask } from "../program-decoder/mask";
 import { terminationInstructions } from "./is-termination-instruction";
 
 export class BasicBlocks {
-  private code: Uint8Array = new Uint8Array();
-  private mask: Mask = Mask.empty();
   private basicBlocks: Set<number> = new Set();
 
   reset(code: Uint8Array, mask: Mask) {
-    this.code = code;
-    this.mask = mask;
-
-    this.calculateBasicBlocks();
-  }
-
-  private calculateBasicBlocks() {
     this.basicBlocks.clear();
     this.basicBlocks.add(0);
-    const codeLength = this.code.length;
+    const codeLength = code.length;
+
+    const isBasicBlockTermination = (index: number) =>
+      mask.isInstruction(index) && terminationInstructions[code[index]];
 
     for (let i = 0; i < codeLength; i++) {
-      if (this.mask.isInstruction(i) && this.isBasicBlockTermination(i)) {
-        this.basicBlocks.add(i + 1 + this.mask.getNoOfBytesToNextInstruction(i + 1));
+      if (mask.isInstruction(i) && isBasicBlockTermination(i)) {
+        this.basicBlocks.add(i + 1 + mask.getNoOfBytesToNextInstruction(i + 1));
       }
     }
   }
 
   isBeginningOfBasicBlock(index: number) {
     return this.basicBlocks.has(index);
-  }
-
-  private isBasicBlockTermination(index: number) {
-    return this.mask.isInstruction(index) && terminationInstructions[this.code[index]];
   }
 }
