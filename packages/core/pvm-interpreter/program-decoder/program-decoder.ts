9ea78eed8aa8bc4aba8d908e165d8069cb40de2a
Mateusz Sikora <ms1qaz@gmail.com>
2025-04-16 15:03:13 +0200

    remove test-runner dependency from pvm

--- packages/core/pvm-interpreter/program-decoder/program-decoder.ts
+++ packages/core/pvm-interpreter/program-decoder/program-decoder.ts
@@ -1,8 +1,10 @@
 import { Decoder } from "@typeberry/codec";
 import { Result } from "@typeberry/utils";
-import { logger } from "../../../../bin/test-runner/common";
 import { JumpTable } from "./jump-table";
 import { Mask } from "./mask";
+import { Logger } from "@typeberry/logger";
+
+const logger = Logger.new(__filename, "pvm-interpreter");
 
 export enum ProgramDecoderError {
   InvalidProgramError = 0,
