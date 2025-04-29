55d0c38fb8c1e28eaedc9212bc396b0a76804184
Mateusz Sikora <ms1qaz@gmail.com>
2025-04-29 23:37:19 +0200

    Apply suggestions from code review
    Co-authored-by: Tomek Drwięga <tomusdrw@users.noreply.github.com>

--- packages/core/pvm-interpreter/memory/page-range.ts
+++ packages/core/pvm-interpreter/memory/page-range.ts
@@ -3,7 +3,16 @@ import type { MemoryRange } from "./memory-range";
 import { getPageNumber } from "./memory-utils";
 import { type PageNumber, getNextPageNumber, tryAsPageNumber } from "./pages/page-utils";
 
+/**
+ * A half-open range of memory pages. Similarly to [`MemoryRange`], can be empty
+ * or wrapped around the memory.
+ */
 export class PageRange {
+  /**
+   * Exclusive end of the page range.
+   * 
+   * NOTE: it may be smaller than `start` in case the range is wrapped.
+   */
   public readonly end: PageNumber;
 
   private constructor(
@@ -13,7 +22,12 @@ export class PageRange {
     this.end = tryAsPageNumber((this.start + this.length) % MAX_NUMBER_OF_PAGES);
   }
 
-  /** Creates range of pages that are part of given memory range */
+  /**
+   * Creates range of pages that includes all indices in a given memory range.
+   * 
+   * Example: For memory range `[PAGE_SIZE, MEMORY_SIZE - PAGE_SIZE)`
+   *                  we will get `[1, MAX_NUMBER_OF_PAGES - 1)` page range. 
+   */
   static fromMemoryRange(range: MemoryRange) {
     const startPage = getPageNumber(range.start);
 
