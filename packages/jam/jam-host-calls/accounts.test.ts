f0da1f2d008ee278aa906687803f23fea97947d7
DrEverr <ms1997sokol@gmail.com>
2025-05-12 11:53:45 +0200

    feat: add Accounts interface and TestAccounts implementation

---
+++ packages/jam/jam-host-calls/accounts.test.ts
@@ -0,0 +1,32 @@
+import type { ServiceId } from "@typeberry/block";
+import type { BytesBlob } from "@typeberry/bytes";
+import { MultiMap } from "@typeberry/collections";
+import type { Blake2bHash } from "@typeberry/hash";
+import type { Accounts } from "./accounts";
+
+export class TestAccounts implements Accounts {
+  public readonly preimages: MultiMap<[ServiceId, Blake2bHash], BytesBlob | null> = new MultiMap(2, [
+    null,
+    (hash) => hash.toString(),
+  ]);
+  public readonly storage: MultiMap<[ServiceId, Blake2bHash], BytesBlob | null> = new MultiMap(2, [
+    null,
+    (hash) => hash.toString(),
+  ]);
+
+  lookup(serviceId: ServiceId, hash: Blake2bHash): Promise<BytesBlob | null> {
+    const preImage = this.preimages.get(serviceId, hash);
+    if (preImage === undefined) {
+      return Promise.resolve(null);
+    }
+    return Promise.resolve(preImage);
+  }
+
+  read(serviceId: ServiceId, hash: Blake2bHash): Promise<BytesBlob | null> {
+    const d = this.storage.get(serviceId, hash);
+    if (d === undefined) {
+      return Promise.resolve(null);
+    }
+    return Promise.resolve(d);
+  }
+}
---
+++ packages/jam/jam-host-calls/accounts.ts
@@ -0,0 +1,19 @@
+import type { ServiceId } from "@typeberry/block";
+import type { BytesBlob } from "@typeberry/bytes";
+import type { Blake2bHash } from "@typeberry/hash";
+
+/**
+ * Account data interface for host calls.
+ *
+ * https://graypaper.fluffylabs.dev/#/9a08063/106001106001?v=0.6.6
+ */
+export interface Accounts {
+  /** Lookup a preimage. */
+  lookup(serviceId: ServiceId, hash: Blake2bHash): Promise<BytesBlob | null>;
+  /**
+   * Read service storage.
+   *
+   * If `serviceId === currentServiceId` we should read from snapshot state.
+   */
+  read(serviceId: ServiceId, hash: Blake2bHash): Promise<BytesBlob | null>;
+}
