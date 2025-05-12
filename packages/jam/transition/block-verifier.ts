ff92252a3025b3848393f1ad1bbeb171ec13fa34
Marcin Sokołowski <ms1997sokol@gmail.com>
2025-05-12 09:04:56 +0200

    Block Verification (#361)
    * Refactor BlockVerifier to include time slot validation and parent block checks
    
    * refactor
    
    * Implement extrinsic hash validation in BlockVerifier and enhance TransitionHasher with extrinsicHash method
    
    * qa-fix
    
    * Enhance BlockVerifier initialization and update verifyBlock method to include timeSlot parameter
    
    * Add .mdb files to .gitignore for database exclusion
    
    * Refactor hashBytes calls to include allocator for improved memory management
    
    * Fix checking timeslot without conversion to seconds
    
    * refactor: projecting guaranteeViewField to guaranteeView
    
    * qa-fix
    
    * refactor: remove unused timeslot parameter from block verification methods, cleanup extrinsic merkle commitment hash
    
    * validate posterior state root in block verification
    
    * fix: include allocator in guarantee hash
    
    * rename to block-verifier
    
    * fix rename import to block-verifier

---
+++ packages/jam/transition/block-verifier.ts
@@ -0,0 +1,71 @@
+import type { BlockView, HeaderHash } from "@typeberry/block";
+import type { BlocksDb } from "@typeberry/database";
+import { Result } from "@typeberry/utils";
+import type { TransitionHasher } from "./hasher";
+
+export enum BlockVerifierError {
+  ParentNotFound = 0,
+  InvalidTimeSlot = 1,
+  InvalidExtrinsic = 2,
+  InvalidStateRoot = 3,
+}
+
+export class BlockVerifier {
+  constructor(
+    public readonly hasher: TransitionHasher,
+    private readonly blocks: BlocksDb,
+  ) {}
+
+  async verifyBlock(block: BlockView): Promise<Result<HeaderHash, BlockVerifierError>> {
+    const headerView = block.header.view();
+
+    // Check if parent block exists.
+    // https://graypaper.fluffylabs.dev/#/cc517d7/0c82000c8200?v=0.6.5
+    // https://graypaper.fluffylabs.dev/#/cc517d7/0c9d000c9d00?v=0.6.5
+    const parentHash = headerView.parentHeaderHash.materialize();
+    const parentBlock = this.blocks.getHeader(parentHash);
+    if (parentBlock === null) {
+      return Result.error(BlockVerifierError.ParentNotFound, `Parent ${parentHash.toString()} not found`);
+    }
+
+    // Check if the time slot index is consecutive and not from future.
+    // https://graypaper.fluffylabs.dev/#/cc517d7/0c02010c0201?v=0.6.5
+    const timeslot = headerView.timeSlotIndex.materialize();
+    const parentTimeslot = parentBlock.timeSlotIndex.materialize();
+    if (timeslot <= parentTimeslot) {
+      return Result.error(
+        BlockVerifierError.InvalidTimeSlot,
+        `Invalid time slot index: ${timeslot}, expected > ${parentTimeslot}}`,
+      );
+    }
+
+    // Check if extrinsic is valid.
+    // https://graypaper.fluffylabs.dev/#/cc517d7/0cba000cba00?v=0.6.5
+    const extrinsicHash = headerView.extrinsicHash.materialize();
+    const extrinsicMerkleCommitment = this.hasher.extrinsic(block.extrinsic.view());
+    if (!extrinsicHash.isEqualTo(extrinsicMerkleCommitment.hash)) {
+      return Result.error(
+        BlockVerifierError.InvalidExtrinsic,
+        `Invalid extrinsic hash: ${extrinsicHash.toString()}, expected ${extrinsicMerkleCommitment.hash.toString()}`,
+      );
+    }
+
+    // Check if the state root is valid.
+    // https://graypaper.fluffylabs.dev/#/cc517d7/0c18010c1801?v=0.6.5
+    const stateRoot = headerView.priorStateRoot.materialize();
+    const posteriorStateRoot = this.blocks.getPostStateRoot(parentHash);
+    if (posteriorStateRoot === null) {
+      return Result.error(BlockVerifierError.ParentNotFound, `Posterior state root ${parentHash.toString()} not found`);
+    }
+    if (stateRoot !== posteriorStateRoot) {
+      return Result.error(
+        BlockVerifierError.InvalidStateRoot,
+        `Invalid state root: ${stateRoot.toString()}, expected ${posteriorStateRoot.toString()}`,
+      );
+    }
+
+    const headerHash = this.hasher.header(headerView);
+
+    return Result.ok(headerHash.hash);
+  }
+}
