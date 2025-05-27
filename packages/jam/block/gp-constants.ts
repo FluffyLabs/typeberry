09f8c2859f4539fe5b09ef0ece0959188db34a5b
Mateusz Sikora <ms1qaz@gmail.com>
2025-05-27 15:45:07 +0200

    remove consts

--- packages/jam/block/gp-constants.ts
+++ packages/jam/block/gp-constants.ts
@@ -14,80 +14,23 @@ import { MAX_NUMBER_OF_WORK_ITEMS } from "./work-package";
  * https://graypaper.fluffylabs.dev/#/579bd12/413000413000
  */
 
-/** `B_I`: The additional minimum balance required per item of elective service state. */
-export const B_I = 10;
-
-/** `B_L`: The additional minimum balance required per octet of elective service state. */
-export const B_L = 1;
-
-/** `B_S`: The basic minimum balance which all services require. */
-export const B_S = 100;
-
-/** `C`: The total number of cores. */
-export const C = 341;
-
-/** `D`: The period in timeslots after which an unreferenced preimage may be expunged. */
-export const D = 19_200;
-
-/** `E`: The length of an epoch in timeslots. */
-export const E = 600;
-
-/** `G_A`: The gas allocated to invoke a work-report’s Accumulation logic. */
-export const G_A = 10_000_000;
-
-/** `G_I`: The gas allocated to invoke a work-package’s Is-Authorized logic. */
-export const G_I = 50_000_000;
-
-/** `G_R`: The gas allocated to invoke a work-package’s Refine logic. */
-export const G_R = 5_000_000_000;
-
-/** `G_T`: The total gas allocated across all Accumulation. */
-export const G_T = 3_500_000_000;
-
 /** `H`: The size of recent history, in blocks. */
 export const H = 8;
 
 /** `I`: Maximum number of work items in a package. */
 export const I = MAX_NUMBER_OF_WORK_ITEMS;
 
-/** `J`: The maximum sum of dependency items in a work-report. */
-export const J = 8;
-
 /** `K`: Max number of tickets which may be submitted in a single extrinsic. */
 export const K = MAX_NUMBER_OF_TICKETS;
-
-/** `L`: The maximum age in timeslots of the lookup anchor. */
-export const L = 14_400;
 
 /** `O`: Maximum number of items in the authorizations pool. */
 export const O = 8;
 
-/** `P`: The slot period, in seconds. */
-export const P = 6;
-
 /** `Q`: The number of items in the authorizations queue. */
 export const Q = 80;
 
-/** `R`: The rotation period of validator-core assignments, in timeslots. */
-export const R = 10;
-
-/** `S`: The maximum number of entries in the accumulation queue. */
-export const S = 1024;
-
 /** `T`: The maximum number of extrinsics in a work-package. */
 export const T = 128;
-
-/** `U`: The period in timeslots after which reported but unavailable work may be replaced. */
-export const U = 5;
-
-/** `V`: The total number of validators. */
-export const V = 1023;
-
-/** `W_A`: The maximum size of is-authorized code in octets. */
-export const W_A = 64_000;
-
-/** `W_B`: The maximum size of an encoded work-package with extrinsic data and imports. */
-export const W_B = 12 * 2 ** 20;
 
 /** `W_C`: The maximum size of service code in octets. */
 export const W_C = 4_000_000;
