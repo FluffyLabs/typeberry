df53394860aee10e816317dce2334dbafa275b61
DrEverr <ms1997sokol@gmail.com>
2025-06-11 22:24:22 +0200

    update bandersnatch documentation

--- packages/core/crypto/bandersnatch.ts
+++ packages/core/crypto/bandersnatch.ts
@@ -2,23 +2,25 @@ import { derive_public_key } from "@fluffylabs/bandersnatch";
 import { Bytes } from "@typeberry/bytes";
 import type { Opaque } from "@typeberry/utils";
 
+/** Bandersnatch public key size. */
 export const BANDERSNATCH_KEY_BYTES = 32;
-export const BLS_KEY_BYTES = 144;
-
 export type BANDERSNATCH_KEY_BYTES = typeof BANDERSNATCH_KEY_BYTES;
+
+/** BLS public key size. */
+export const BLS_KEY_BYTES = 144;
 export type BLS_KEY_BYTES = typeof BLS_KEY_BYTES;
 
 /**
  * Potentially valid Bandersnatch public key.
  *
- * https://graypaper.fluffylabs.dev/#/579bd12/082200082200
+ * https://graypaper.fluffylabs.dev/#/7e6ff6a/082200082200?v=0.6.7
  */
 export type BandersnatchKey = Opaque<Bytes<BANDERSNATCH_KEY_BYTES>, "BandersnatchKey">;
 
 /**
  * A public key for BLS signature scheme
  *
- * https://graypaper.fluffylabs.dev/#/579bd12/081c00081c00
+ * https://graypaper.fluffylabs.dev/#/7e6ff6a/081c00081c00?v=0.6.7
  */
 export type BlsKey = Opaque<Bytes<BLS_KEY_BYTES>, "BlsKey">;
 
