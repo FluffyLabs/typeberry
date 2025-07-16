34a321457c494626c610f04fbe661f3a983d1939
Sebastian Koszuta <sebastian.koszuta@gmail.com>
2025-07-16 08:48:25 +0200

    separate script for davxy vectors

--- bin/test-runner/w3f-davxy.ts
+++ bin/test-runner/w3f-davxy.ts
@@ -1,7 +1,7 @@
 import { logger, main } from "./common.js";
 import { runners } from "./w3f/runners.js";
 
-main(runners, process.argv.slice(2), "test-vectors/w3f-fluffy")
+main(runners, process.argv.slice(2), "test-vectors/w3f-davxy", { ignored: ["genesis.json"] })
   .then((r) => logger.log(r))
   .catch((e) => {
     logger.error(`${e}`);
---
+++ bin/test-runner/w3f/runners.ts
@@ -0,0 +1,85 @@
+import {
+  blockFromJson,
+  disputesExtrinsicFromJson,
+  getAssurancesExtrinsicFromJson,
+  getExtrinsicFromJson,
+  guaranteesExtrinsicFromJson,
+  headerFromJson,
+  preimagesExtrinsicFromJson,
+  refineContextFromJson,
+  ticketsExtrinsicFromJson,
+  workReportFromJson,
+  workResultFromJson,
+} from "@typeberry/block-json";
+import { tinyChainSpec } from "@typeberry/config";
+import { runner } from "../common.js";
+import { AccumulateTest, runAccumulateTest } from "./accumulate.js";
+import { AssurancesTestFull, AssurancesTestTiny, runAssurancesTestFull, runAssurancesTestTiny } from "./assurances.js";
+import { AuthorizationsTest, runAuthorizationsTest } from "./authorizations.js";
+import {
+  runAssurancesExtrinsicTest,
+  runBlockTest,
+  runDisputesExtrinsicTest,
+  runExtrinsicTest,
+  runGuaranteesExtrinsicTest,
+  runHeaderTest,
+  runPreimagesExtrinsicTest,
+  runRefineContextTest,
+  runTicketsExtrinsicTest,
+  runWorkReportTest,
+  runWorkResultTest,
+} from "./codec/index.js";
+import { runWorkItemTest, workItemFromJson } from "./codec/work-item.js";
+import { runWorkPackageTest, workPackageFromJson } from "./codec/work-package.js";
+import { DisputesTest, runDisputesTest } from "./disputes.js";
+import { EcTest, runEcTest } from "./erasure-coding.js";
+import { HostCallAccumulateTest, runHostCallAccumulateTest } from "./host-calls-accumulate.js";
+import { HostCallGeneralTest, runHostCallGeneralTest } from "./host-calls-general.js";
+import { HostCallRefineTest, runHostCallRefineTest } from "./host-calls-refine.js";
+import { PreImagesTest, runPreImagesTest } from "./preimages.js";
+import { PvmTest, runPvmTest } from "./pvm.js";
+import { HistoryTest, runHistoryTest } from "./recent-history.js";
+import { ReportsTest, runReportsTestFull, runReportsTestTiny } from "./reports.js";
+import { SafroleTest, runSafroleTest } from "./safrole.js";
+import { JsonSchema, ignoreSchemaFiles } from "./schema.js";
+import { runShufflingTests, shufflingTests } from "./shuffling.js";
+import { StateTransition, runStateTransition } from "./state-transition.js";
+import { StatisticsTestFull, StatisticsTestTiny, runStatisticsTestFull, runStatisticsTestTiny } from "./statistics.js";
+import { runTrieTest, trieTestSuiteFromJson } from "./trie.js";
+
+export const runners = [
+  runner("accumulate", AccumulateTest.fromJson, runAccumulateTest),
+  runner("assurances/tiny", AssurancesTestTiny.fromJson, runAssurancesTestTiny),
+  runner("assurances/full", AssurancesTestFull.fromJson, runAssurancesTestFull),
+  runner("authorizations", AuthorizationsTest.fromJson, runAuthorizationsTest),
+  runner("codec/assurances_extrinsic", getAssurancesExtrinsicFromJson(tinyChainSpec), runAssurancesExtrinsicTest),
+  runner("codec/block", blockFromJson(tinyChainSpec), runBlockTest),
+  runner("codec/disputes_extrinsic", disputesExtrinsicFromJson, runDisputesExtrinsicTest),
+  runner("codec/extrinsic", getExtrinsicFromJson(tinyChainSpec), runExtrinsicTest),
+  runner("codec/guarantees_extrinsic", guaranteesExtrinsicFromJson, runGuaranteesExtrinsicTest),
+  runner("codec/header", headerFromJson, runHeaderTest),
+  runner("codec/preimages_extrinsic", preimagesExtrinsicFromJson, runPreimagesExtrinsicTest),
+  runner("codec/refine_context", refineContextFromJson, runRefineContextTest),
+  runner("codec/tickets_extrinsic", ticketsExtrinsicFromJson, runTicketsExtrinsicTest),
+  runner("codec/work_item", workItemFromJson, runWorkItemTest),
+  runner("codec/work_package", workPackageFromJson, runWorkPackageTest),
+  runner("codec/work_report", workReportFromJson, runWorkReportTest),
+  runner("codec/work_result", workResultFromJson, runWorkResultTest),
+  runner("disputes", DisputesTest.fromJson, runDisputesTest),
+  runner("erasure_coding", EcTest.fromJson, runEcTest),
+  runner("history", HistoryTest.fromJson, runHistoryTest),
+  runner("schema", JsonSchema.fromJson, ignoreSchemaFiles), // ignore schema files
+  runner("preimages", PreImagesTest.fromJson, runPreImagesTest),
+  runner("pvm", PvmTest.fromJson, runPvmTest),
+  runner("host_function", HostCallGeneralTest.fromJson, runHostCallGeneralTest),
+  runner("host_function", HostCallAccumulateTest.fromJson, runHostCallAccumulateTest),
+  runner("host_function", HostCallRefineTest.fromJson, runHostCallRefineTest),
+  runner("reports/tiny", ReportsTest.fromJson, runReportsTestTiny),
+  runner("reports/full", ReportsTest.fromJson, runReportsTestFull),
+  runner("safrole", SafroleTest.fromJson, runSafroleTest),
+  runner("shuffle", shufflingTests, runShufflingTests),
+  runner("statistics/tiny", StatisticsTestTiny.fromJson, runStatisticsTestTiny),
+  runner("statistics/full", StatisticsTestFull.fromJson, runStatisticsTestFull),
+  runner("trie", trieTestSuiteFromJson, runTrieTest),
+  runner("traces", StateTransition.fromJson, runStateTransition),
+];
