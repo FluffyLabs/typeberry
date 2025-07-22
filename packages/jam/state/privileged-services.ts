a07f973727aaa5bf9f29089d93aa212b58225a55
DrEverr <ms1997sokol@gmail.com>
2025-07-22 10:40:25 +0200

    privilege authorization queue change to per core

--- packages/jam/state/privileged-services.ts
+++ packages/jam/state/privileged-services.ts
@@ -1,5 +1,8 @@
 import type { ServiceGas, ServiceId } from "@typeberry/block";
+import { codecWithContext } from "@typeberry/block/codec.js";
 import { type CodecRecord, codec, readonlyArray } from "@typeberry/codec";
+import { Compatibility, GpVersion } from "@typeberry/utils";
+import { type PerCore, codecPerCore, tryAsPerCore } from "./common.js";
 
 /** Dictionary entry of services that auto-accumulate every block. */
 export class AutoAccumulate {
@@ -21,12 +24,22 @@ export class AutoAccumulate {
 }
 
 /**
- * https://graypaper.fluffylabs.dev/#/85129da/11a20111a201?v=0.6.3
+ * https://graypaper.fluffylabs.dev/#/7e6ff6a/11da0111da01?v=0.6.7
  */
 export class PrivilegedServices {
   static Codec = codec.Class(PrivilegedServices, {
     manager: codec.u32.asOpaque<ServiceId>(),
-    authManager: codec.u32.asOpaque<ServiceId>(),
+    authManager: Compatibility.isGreaterOrEqual(GpVersion.V0_6_7)
+      ? codecPerCore(codec.u32.asOpaque<ServiceId>())
+      : codecWithContext((ctx) =>
+          codec.u32.asOpaque<ServiceId>().convert(
+            (perCore: PerCore<ServiceId>) => perCore[0],
+            (serviceId: ServiceId) => {
+              const array = new Array(ctx.coresCount).fill(serviceId);
+              return tryAsPerCore(array, ctx);
+            },
+          ),
+        ),
     validatorsManager: codec.u32.asOpaque<ServiceId>(),
     autoAccumulateServices: readonlyArray(codec.sequenceVarLen(AutoAccumulate.Codec)),
   });
@@ -38,12 +51,13 @@ export class PrivilegedServices {
   private constructor(
     /**
      * `chi_m`: The first, χm, is the index of the manager service which is
-     * the service able to effect an alteration of χ from block to block.
-     * https://graypaper.fluffylabs.dev/#/85129da/117201117501?v=0.6.3
+     * the service able to effect an alteration of χ from block to block,
+     * as well as bestow services with storage deposit credits.
+     * https://graypaper.fluffylabs.dev/#/7e6ff6a/11a40111a801?v=0.6.7
      */
     readonly manager: ServiceId,
-    /** `chi_a`: Manages authorization queue. */
-    readonly authManager: ServiceId,
+    /** `chi_a`: Manages authorization queue one for each core. */
+    readonly authManager: PerCore<ServiceId>,
     /** `chi_v`: Managers validator keys. */
     readonly validatorsManager: ServiceId,
     /** `chi_g`: Dictionary of services that auto-accumulate every block with their gas limit. */
