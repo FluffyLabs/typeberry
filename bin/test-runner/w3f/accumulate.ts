b2606e0eae8d8a5589a2a23a58dd957ef83dca89
DrEverr <ms1997sokol@gmail.com>
2025-07-22 11:04:35 +0200

    privilege authorization queue change to per core

--- bin/test-runner/w3f/accumulate.ts
+++ bin/test-runner/w3f/accumulate.ts
@@ -1,16 +1,16 @@
 import assert from "node:assert";
-import { type EntropyHash, type TimeSlot, tryAsPerEpochBlock, tryAsServiceGas, tryAsServiceId } from "@typeberry/block";
+import { type EntropyHash, ServiceGas, ServiceId, type TimeSlot, tryAsPerEpochBlock, tryAsServiceGas, tryAsServiceId } from "@typeberry/block";
 import { fromJson, workReportFromJson } from "@typeberry/block-json";
 import type { WorkPackageHash, WorkReport } from "@typeberry/block/work-report.js";
 import { HashSet, asKnownSize } from "@typeberry/collections";
 import type { ChainSpec } from "@typeberry/config";
 import { type FromJson, json } from "@typeberry/json-parser";
 import type { InMemoryService } from "@typeberry/state";
-import { AutoAccumulate, InMemoryState, PrivilegedServices } from "@typeberry/state";
+import { AutoAccumulate, InMemoryState, PrivilegedServices, tryAsPerCore } from "@typeberry/state";
 import { JsonService } from "@typeberry/state-json/accounts.js";
 import { NotYetAccumulatedReport } from "@typeberry/state/not-yet-accumulated.js";
 import { Accumulate, type AccumulateRoot } from "@typeberry/transition/accumulate/index.js";
-import { Result, deepEqual } from "@typeberry/utils";
+import { Compatibility, GpVersion, Result, deepEqual } from "@typeberry/utils";
 import { getChainSpec } from "./spec.js";
 
 class Input {
@@ -25,6 +25,76 @@ class Input {
 
 class TestState {
   static fromJson = json.object<TestState, TestState>(
+    {
+      slot: "number",
+      entropy: fromJson.bytes32(),
+      ready_queue: [
+        "array",
+        json.array({
+          report: workReportFromJson,
+          dependencies: json.array(fromJson.bytes32()),
+        }),
+      ],
+      accumulated: ["array", json.array(fromJson.bytes32())],
+      privileges: {
+        bless: "number",
+        assign: json.array("number"),
+        designate: "number",
+        always_acc: json.array({
+          id: "number",
+          gas: json.fromNumber((x) => tryAsServiceGas(x)),
+        }),
+      },
+      accounts: json.array(JsonService.fromJson),
+    },
+    (x) => x,
+  );
+
+  slot!: TimeSlot;
+  entropy!: EntropyHash;
+  ready_queue!: { report: WorkReport; dependencies: WorkPackageHash[] }[][];
+  accumulated!: WorkPackageHash[][];
+  privileges!: {
+    bless: ServiceId;
+    assign: Array<ServiceId>;
+    designate: ServiceId;
+    always_acc: { id: ServiceId; gas: ServiceGas }[];
+  };
+  accounts!: InMemoryService[];
+
+  static toAccumulateState(
+    { accounts, slot, ready_queue, accumulated, privileges }: TestState,
+    chainSpec: ChainSpec,
+  ): InMemoryState {
+    return InMemoryState.partial(chainSpec, {
+      timeslot: slot,
+      accumulationQueue: tryAsPerEpochBlock(
+        ready_queue.map((queue) =>
+          queue.map((item) =>
+            NotYetAccumulatedReport.create({ report: item.report, dependencies: asKnownSize(item.dependencies) }),
+          ),
+        ),
+        chainSpec,
+      ),
+      recentlyAccumulated: tryAsPerEpochBlock(
+        accumulated.map((queue) => HashSet.from(queue)),
+        chainSpec,
+      ),
+      privilegedServices: PrivilegedServices.create({
+        manager: privileges.bless,
+        authManager: tryAsPerCore(privileges.assign, chainSpec),
+        validatorsManager: privileges.designate,
+        autoAccumulateServices: privileges.always_acc.map(({ gas, id }) =>
+          AutoAccumulate.create({ gasLimit: gas, service: id }),
+        ),
+      }),
+      services: new Map(accounts.map((service) => [service.serviceId, service])),
+    });
+  }
+}
+
+class TestStatePre067 {
+  static fromJson = json.object<TestStatePre067, TestStatePre067>(
     {
       slot: "number",
       entropy: fromJson.bytes32(),
@@ -63,7 +133,7 @@ class TestState {
   accounts!: InMemoryService[];
 
   static toAccumulateState(
-    { accounts, slot, ready_queue, accumulated, privileges }: TestState,
+    { accounts, slot, ready_queue, accumulated, privileges }: TestStatePre067,
     chainSpec: ChainSpec,
   ): InMemoryState {
     return InMemoryState.partial(chainSpec, {
@@ -82,7 +152,7 @@ class TestState {
       ),
       privilegedServices: PrivilegedServices.create({
         manager: tryAsServiceId(privileges.bless),
-        authManager: tryAsServiceId(privileges.assign),
+        authManager: tryAsPerCore(new Array(chainSpec.coresCount).fill(tryAsServiceId(privileges.assign)), chainSpec),
         validatorsManager: tryAsServiceId(privileges.designate),
         autoAccumulateServices: privileges.always_acc.map(({ gas, id }) =>
           AutoAccumulate.create({ gasLimit: tryAsServiceGas(gas), service: tryAsServiceId(id) }),
@@ -108,15 +178,19 @@ class Output {
 export class AccumulateTest {
   static fromJson: FromJson<AccumulateTest> = {
     input: Input.fromJson,
-    pre_state: TestState.fromJson,
+    pre_state: Compatibility.isGreaterOrEqual(GpVersion.V0_6_7)
+      ? TestState.fromJson
+      : TestStatePre067.fromJson,
     output: Output.fromJson,
-    post_state: TestState.fromJson,
+    post_state: Compatibility.isGreaterOrEqual(GpVersion.V0_6_7)
+      ? TestState.fromJson
+      : TestStatePre067.fromJson,
   };
 
   input!: Input;
-  pre_state!: TestState;
+  pre_state!: TestState | TestStatePre067;
   output!: Output;
-  post_state!: TestState;
+  post_state!: TestState | TestStatePre067;
 }
 
 export async function runAccumulateTest(test: AccumulateTest, path: string) {
@@ -130,7 +204,9 @@ export async function runAccumulateTest(test: AccumulateTest, path: string) {
    */
   const entropy = test.pre_state.entropy;
 
-  const state = TestState.toAccumulateState(test.pre_state, chainSpec);
+  const state = Compatibility.isGreaterOrEqual(GpVersion.V0_6_7)
+    ? TestState.toAccumulateState(test.pre_state as TestState, chainSpec)
+    : TestStatePre067.toAccumulateState(test.pre_state as TestStatePre067, chainSpec);
   const accumulate = new Accumulate(chainSpec, state);
   const result = await accumulate.transition({ ...test.input, entropy });
 
@@ -140,6 +216,10 @@ export async function runAccumulateTest(test: AccumulateTest, path: string) {
 
   state.applyUpdate(result.ok.stateUpdate);
 
-  deepEqual(state, TestState.toAccumulateState(test.post_state, chainSpec));
+  const post_state = Compatibility.isGreaterOrEqual(GpVersion.V0_6_7)
+    ? TestState.toAccumulateState(test.post_state as TestState, chainSpec)
+    : TestStatePre067.toAccumulateState(test.post_state as TestStatePre067, chainSpec);
+
+  deepEqual(state, post_state);
   deepEqual(result.ok.root, test.output.ok);
 }
