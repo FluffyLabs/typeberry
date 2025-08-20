f1627c470fb8a112a5214962c31092848a0969fb
Mateusz Sikora <ms1qaz@gmail.com>
2025-08-20 11:19:42 +0200

    subtract additional 10 gas in gas host call

--- bin/test-runner/jamduna-065.ts
+++ bin/test-runner/jamduna-065.ts
@@ -6,22 +6,10 @@ const runners = [runner("state_transition", StateTransition.fromJson, runStateTr
 main(runners, process.argv.slice(2), "test-vectors/jamduna_065", {
   accepted: ["generic/state_transitions/", "assurances/state_transitions/", "orderedaccumulation/state_transitions/"],
   ignored: [
-    "assurances/state_transitions/00000026.json",
-    "assurances/state_transitions/00000028.json",
-    "assurances/state_transitions/00000030.json",
-    "assurances/state_transitions/00000032.json",
     "assurances/state_transitions/00000035.json",
     "assurances/state_transitions/00000037.json",
-    "assurances/state_transitions/00000039.json",
-    "assurances/state_transitions/00000041.json",
     "assurances/state_transitions/00000043.json",
     "assurances/state_transitions/00000047.json",
-    "orderedaccumulation/state_transitions/00000029.json",
-    "orderedaccumulation/state_transitions/00000031.json",
-    "orderedaccumulation/state_transitions/00000033.json",
-    "orderedaccumulation/state_transitions/00000035.json",
-    "orderedaccumulation/state_transitions/00000037.json",
-    "orderedaccumulation/state_transitions/00000039.json",
   ],
 })
   .then((r) => logger.log(r))
