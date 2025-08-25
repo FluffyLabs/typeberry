b3421b5ca619be0f00dda2b8de3d600b32fffeab
Sebastian Koszuta <sebastian.koszuta@gmail.com>
2025-08-25 12:02:49 +0200

    added test runner for davxy 0.6.7, added expunge period compatibility for davxy vectors

--- bin/test-runner/w3f-davxy-066.ts
+++ bin/test-runner/w3f-davxy-066.ts
@@ -1,7 +1,7 @@
 import { logger, main } from "./common.js";
 import { runners } from "./w3f/runners.js";
 
-main(runners, process.argv.slice(2), "test-vectors/w3f-davxy", {
+main(runners, process.argv.slice(2), "test-vectors/w3f-davxy_066", {
   accepted: ["traces/safrole", "traces/fallback"],
   ignored: ["genesis.json"],
 })
--- packages/jam/config/chain-spec.ts
+++ packages/jam/config/chain-spec.ts
@@ -1,4 +1,4 @@
-import { WithDebug } from "@typeberry/utils";
+import { Compatibility, TestSuite, WithDebug } from "@typeberry/utils";
 
 /**
  * Estimated number of validators.
@@ -98,7 +98,7 @@ export const tinyChainSpec = new ChainSpec({
   ticketsPerValidator: 3,
   validatorsCount: 6,
   numberECPiecesPerSegment: 1026,
-  preimageExpungePeriod: 6,
+  preimageExpungePeriod: Compatibility.isSuite(TestSuite.W3F_DAVXY) ? 32 : 6, // https://github.com/davxy/jam-test-vectors/tree/v0.6.6/traces#preimage-expunge-delay
 });
 
 /**
