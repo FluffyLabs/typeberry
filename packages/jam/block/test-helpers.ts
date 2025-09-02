ed33cba3967b37d7c983f3efca1e61b4c3205aa8
Sebastian Koszuta <sebastian.koszuta@gmail.com>
2025-09-02 08:06:21 +0200

    removed 0.6.4 remnants, updated GP_VERSION constants

--- packages/jam/block/test-helpers.ts
+++ packages/jam/block/test-helpers.ts
@@ -2,10 +2,8 @@ import { BytesBlob } from "@typeberry/bytes";
 import { Decoder } from "@typeberry/codec";
 import { tinyChainSpec } from "@typeberry/config";
 import { Block } from "./block.js";
-import testBlockData_0_6_4 from "./test-block-0-6-4.js";
 import testBlockData_0_6_5 from "./test-block-0-6-5.js";
 import testBlockData from "./test-block.js";
-import testWorkReportData_0_6_4 from "./test-work-report-0-6-4.js";
 import testWorkReportData from "./test-work-report.js";
 
 import { Compatibility, GpVersion } from "@typeberry/utils";
@@ -14,10 +12,7 @@ export function testBlockHex() {
   if (Compatibility.isGreaterOrEqual(GpVersion.V0_7_0)) {
     return testBlockData;
   }
-  if (Compatibility.isGreaterOrEqual(GpVersion.V0_6_5)) {
-    return testBlockData_0_6_5;
-  }
-  return testBlockData_0_6_4;
+  return testBlockData_0_6_5;
 }
 export function testBlock() {
   return Decoder.decodeObject(Block.Codec, BytesBlob.parseBlob(testBlockHex()), tinyChainSpec);
@@ -28,5 +23,5 @@ export function testBlockView() {
 }
 
 export function testWorkReportHex() {
-  return Compatibility.isGreaterOrEqual(GpVersion.V0_6_5) ? testWorkReportData : testWorkReportData_0_6_4;
+  return testWorkReportData;
 }
