0b7b5d6191258c3094ea8de2d3b22abe3e6bd1ba
Tomasz Drwięga <tomusdrw@gmail.com>
2025-09-05 00:16:29 +0200

    Fix biome ignore.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,7 @@ export default tseslint.config(
   { plugins: baseImport.plugins },
   {
     files: ["**/*.ts"],
-    ignores: ["dist/**", "tools/builder/pkg.ts"],
+    ignores: ["dist/*", "dist/**", "tools/builder/pkg.ts"],
     languageOptions: {
       parserOptions: {
         projectService: true,
