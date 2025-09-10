77691488d50b484873eea76df1ee75185cb65d16
Mateusz Sikora <ms1qaz@gmail.com>
2025-09-10 14:52:01 +0200

    Update bin/test-runner/w3f.ts
    Co-authored-by: Tomek Drwięga <tomusdrw@users.noreply.github.com>

--- bin/test-runner/w3f.ts
+++ bin/test-runner/w3f.ts
@@ -7,6 +7,7 @@ main(runners, process.argv.slice(2), "test-vectors/w3f-fluffy", {
     // TODO [ToDr] Erasure coding test vectors need to be updated to GP 0.7.0
     "erasure/",
     // Ignored due to incorrect expected gas
+    // https://paritytech.github.io/matrix-archiver/archive/_21ddsEwXlCWnreEGuqXZ_3Apolkadot.io/index.html#$qvS25IbmiyGNWR0kuxhZpukDWP5H7d_5rmUiEJ7KTUI
     "pvm/programs/inst_load_u8_nok.json",
     "pvm/programs/inst_store_imm_indirect_u16_with_offset_nok.json",
     "pvm/programs/inst_store_imm_indirect_u32_with_offset_nok.json",
