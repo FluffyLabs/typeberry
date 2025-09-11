4ea5a48f4b6f59bd6e8eb76442278f560d313a88
Tomasz Drwięga <tomusdrw@gmail.com>
2025-09-11 12:24:24 +0200

    Remove fixed cases.

--- bin/test-runner/jam-conformance-070.ts
+++ bin/test-runner/jam-conformance-070.ts
@@ -18,12 +18,9 @@ main(runners, process.argv.slice(2), "test-vectors/jam-conformance/fuzz-reports/
     // difference in statistics
     "traces/1757422206/00000011.json",
     "traces/1757421952/00000011.json", // note [seko] The block should be rejected, yet we imported it.
-    "traces/1757421101/00000090.json", // note [seko] different storage values, extra item in accumulation output log, different accumulate gas used
     "traces/1757421101/00000091.json", // note [seko] different storage value, extra items in storage, extra item in accumulation output log, different accumulate gas used
-    "traces/1757406558/00000032.json", // note [seko] different storage values, different accumulate gas used
     "traces/1757406516/00000021.json", // note [seko] storage difference, similar to traces/1757092821/00000156.json
     "traces/1757406516/00000022.json", // note [seko] storage difference, similar to traces/1757092821/00000156.json
-    "traces/1757406441/00000116.json", // note [seko] storage differences, different accumulate gas used, extra item in accumulation output log
     // note [seko] log message:
     // LOG   [accumulate] Code with hash 0xbaf736ff7927f6f7dfa744a10a67a48b261ae89bbe5c712f7c1f0ee023776661 not found for service 306895876.
     // accumulate gas used should be 0 but isn't
