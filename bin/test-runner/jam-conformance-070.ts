092f918bcbe75deb2bdbede92c72d894480b951f
Sebastian Koszuta <sebastian.koszuta@gmail.com>
2025-09-11 14:56:13 +0200

    enabled test which are now passing due to recent changes (#605)

--- bin/test-runner/jam-conformance-070.ts
+++ bin/test-runner/jam-conformance-070.ts
@@ -7,10 +7,6 @@ main(runners, process.argv.slice(2), "test-vectors/jam-conformance/fuzz-reports/
     "traces/1757063641/00000180.json", // note [seko] test rejected at block parsing stage, which is considered valid behavior
     "traces/1757062927/00000091.json", // note [seko] block should be rejected but isn't
     "traces/1757423902/00000148.json", // note [seko] major discrepancy. differences in service info, storage, statistics, recent blocks and accumulation output log
-    // note [seko] log message:
-    // LOG   [accumulate] Code with hash 0x0000000000000000000000000000000000000000000000000000000000000000 not found for service 3432010466.
-    // and there's a difference in statistics: our accumulate gas used: 10000000, should be 0.
-    "traces/1757423902/00000152.json",
     "traces/1757423433/00000024.json", // note [seko] The block should be rejected, yet we imported it.
     // note [seko] log message:
     // LOG   [accumulate] Code with hash 0xbaf736ff7927f6f7dfa744a10a67a48b261ae89bbe5c712f7c1f0ee023776661 not found for service 1467575786.
@@ -21,10 +17,6 @@ main(runners, process.argv.slice(2), "test-vectors/jam-conformance/fuzz-reports/
     "traces/1757421101/00000091.json", // note [seko] different storage value, extra items in storage, extra item in accumulation output log, different accumulate gas used
     "traces/1757406516/00000021.json", // note [seko] storage difference, similar to traces/1757092821/00000156.json
     "traces/1757406516/00000022.json", // note [seko] storage difference, similar to traces/1757092821/00000156.json
-    // note [seko] log message:
-    // LOG   [accumulate] Code with hash 0xbaf736ff7927f6f7dfa744a10a67a48b261ae89bbe5c712f7c1f0ee023776661 not found for service 306895876.
-    // accumulate gas used should be 0 but isn't
-    "traces/1757406356/00000019.json",
     "genesis.json",
   ],
 })
