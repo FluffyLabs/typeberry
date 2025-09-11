61e63eca9acc393749335d953fb24fd875434609
Mateusz Sikora <ms1qaz@gmail.com>
2025-09-11 12:30:27 +0200

    Fix statistics (#601)
    * bump reader links
    
    * remove legacy service codec
    
    * fix statistics
    
    * enable fixed test
    
    * fix condition
    
    * remove pre 067 args encoding

--- packages/jam/state-merkleization/serialize.ts
+++ packages/jam/state-merkleization/serialize.ts
@@ -40,7 +40,7 @@ export type StateCodec<T> = {
 
 /** Serialization for particular state entries. */
 export namespace serialize {
-  /** C(1): https://graypaper.fluffylabs.dev/#/85129da/38a20138a201?v=0.6.3 */
+  /** C(1): https://graypaper.fluffylabs.dev/#/7e6ff6a/3b15013b1501?v=0.6.7 */
   export const authPools: StateCodec<State["authPools"]> = {
     key: stateKeys.index(StateKeyIdx.Alpha),
     Codec: codecPerCore(
@@ -53,7 +53,7 @@ export namespace serialize {
     extract: (s) => s.authPools,
   };
 
-  /** C(2): https://graypaper.fluffylabs.dev/#/85129da/38be0138be01?v=0.6.3 */
+  /** C(2): https://graypaper.fluffylabs.dev/#/7e6ff6a/3b31013b3101?v=0.6.7 */
   export const authQueues: StateCodec<State["authQueues"]> = {
     key: stateKeys.index(StateKeyIdx.Phi),
     Codec: codecPerCore(
@@ -64,7 +64,6 @@ export namespace serialize {
 
   /**
    * C(3): Recent blocks with compatibility
-   *  https://graypaper.fluffylabs.dev/#/85129da/38cb0138cb01?v=0.6.3
    *  https://graypaper.fluffylabs.dev/#/7e6ff6a/3b3e013b3e01?v=0.6.7
    */
   export const recentBlocks: StateCodec<State["recentBlocks"]> = {
@@ -73,7 +72,7 @@ export namespace serialize {
     extract: (s) => s.recentBlocks,
   };
 
-  /** C(4): https://graypaper.fluffylabs.dev/#/85129da/38e60138e601?v=0.6.3 */
+  /** C(4): https://graypaper.fluffylabs.dev/#/7e6ff6a/3b63013b6301?v=0.6.7 */
   export const safrole: StateCodec<SafroleData> = {
     key: stateKeys.index(StateKeyIdx.Gamma),
     Codec: SafroleData.Codec,
@@ -86,63 +85,63 @@ export namespace serialize {
       }),
   };
 
-  /** C(5): https://graypaper.fluffylabs.dev/#/85129da/383d02383d02?v=0.6.3 */
+  /** C(5): https://graypaper.fluffylabs.dev/#/7e6ff6a/3bba013bba01?v=0.6.7 */
   export const disputesRecords: StateCodec<State["disputesRecords"]> = {
     key: stateKeys.index(StateKeyIdx.Psi),
     Codec: DisputesRecords.Codec,
     extract: (s) => s.disputesRecords,
   };
 
-  /** C(6): https://graypaper.fluffylabs.dev/#/85129da/387602387602?v=0.6.3 */
+  /** C(6): https://graypaper.fluffylabs.dev/#/7e6ff6a/3bf3013bf301?v=0.6.7 */
   export const entropy: StateCodec<State["entropy"]> = {
     key: stateKeys.index(StateKeyIdx.Eta),
     Codec: codecFixedSizeArray(codec.bytes(HASH_SIZE).asOpaque<EntropyHash>(), ENTROPY_ENTRIES),
     extract: (s) => s.entropy,
   };
 
-  /** C(7): https://graypaper.fluffylabs.dev/#/85129da/388302388302?v=0.6.3 */
+  /** C(7): https://graypaper.fluffylabs.dev/#/7e6ff6a/3b00023b0002?v=0.6.7 */
   export const designatedValidators: StateCodec<State["designatedValidatorData"]> = {
     key: stateKeys.index(StateKeyIdx.Iota),
     Codec: codecPerValidator(ValidatorData.Codec),
     extract: (s) => s.designatedValidatorData,
   };
 
-  /** C(8): https://graypaper.fluffylabs.dev/#/85129da/389002389002?v=0.6.3 */
+  /** C(8): https://graypaper.fluffylabs.dev/#/7e6ff6a/3b0d023b0d02?v=0.6.7 */
   export const currentValidators: StateCodec<State["currentValidatorData"]> = {
     key: stateKeys.index(StateKeyIdx.Kappa),
     Codec: codecPerValidator(ValidatorData.Codec),
     extract: (s) => s.currentValidatorData,
   };
 
-  /** C(9): https://graypaper.fluffylabs.dev/#/85129da/389d02389d02?v=0.6.3 */
+  /** C(9): https://graypaper.fluffylabs.dev/#/7e6ff6a/3b1a023b1a02?v=0.6.7 */
   export const previousValidators: StateCodec<State["previousValidatorData"]> = {
     key: stateKeys.index(StateKeyIdx.Lambda),
     Codec: codecPerValidator(ValidatorData.Codec),
     extract: (s) => s.previousValidatorData,
   };
 
-  /** C(10): https://graypaper.fluffylabs.dev/#/85129da/38aa0238aa02?v=0.6.3 */
+  /** C(10): https://graypaper.fluffylabs.dev/#/7e6ff6a/3b27023b2702?v=0.6.7 */
   export const availabilityAssignment: StateCodec<State["availabilityAssignment"]> = {
     key: stateKeys.index(StateKeyIdx.Rho),
     Codec: codecPerCore(codec.optional(AvailabilityAssignment.Codec)),
     extract: (s) => s.availabilityAssignment,
   };
 
-  /** C(11): https://graypaper.fluffylabs.dev/#/85129da/38c10238c102?v=0.6.3 */
+  /** C(11): https://graypaper.fluffylabs.dev/#/7e6ff6a/3b3e023b3e02?v=0.6.7 */
   export const timeslot: StateCodec<State["timeslot"]> = {
     key: stateKeys.index(StateKeyIdx.Tau),
     Codec: codec.u32.asOpaque<TimeSlot>(),
     extract: (s) => s.timeslot,
   };
 
-  /** C(12): https://graypaper.fluffylabs.dev/#/85129da/38cf0238cf02?v=0.6.3 */
+  /** C(12): https://graypaper.fluffylabs.dev/#/7e6ff6a/3b4c023b4c02?v=0.6.7 */
   export const privilegedServices: StateCodec<State["privilegedServices"]> = {
     key: stateKeys.index(StateKeyIdx.Chi),
     Codec: PrivilegedServices.Codec,
     extract: (s) => s.privilegedServices,
   };
 
-  /** C(13): https://graypaper.fluffylabs.dev/#/85129da/38e10238e102?v=0.6.3 */
+  /** C(13): https://graypaper.fluffylabs.dev/#/7e6ff6a/3b5e023b5e02?v=0.6.7 */
   export const statistics: StateCodec<State["statistics"]> = {
     key: stateKeys.index(StateKeyIdx.Pi),
     Codec: StatisticsData.Codec,
@@ -156,7 +155,7 @@ export namespace serialize {
     extract: (s) => s.accumulationQueue,
   };
 
-  /** C(15): https://graypaper.fluffylabs.dev/#/85129da/381903381903?v=0.6.3 */
+  /** C(15): https://graypaper.fluffylabs.dev/#/7e6ff6a/3b96023b9602?v=0.6.7 */
   export const recentlyAccumulated: StateCodec<State["recentlyAccumulated"]> = {
     key: stateKeys.index(StateKeyIdx.Xi),
     Codec: codecPerEpochBlock(
