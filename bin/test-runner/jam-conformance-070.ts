a564960628a6a301788293019343fd449d4168d6
Sebastian Koszuta <sebastian.koszuta@gmail.com>
2025-09-11 12:49:29 +0200

    enabled test which are now passing due to recent changes

--- bin/test-runner/jam-conformance-070.ts
+++ bin/test-runner/jam-conformance-070.ts
@@ -8,10 +8,6 @@ main(runners, process.argv.slice(2), "test-vectors/jam-conformance/fuzz-reports/
     "traces/1757062927/00000091.json", // note [seko] block should be rejected but isn't
     "traces/1756548741/00000059.json", // note [seko] mismatch in storage bytes used and accumulate gas used
     "traces/1757423902/00000148.json", // note [seko] major discrepancy. differences in service info, storage, statistics, recent blocks and accumulation output log
-    // note [seko] log message:
-    // LOG   [accumulate] Code with hash 0x0000000000000000000000000000000000000000000000000000000000000000 not found for service 3432010466.
-    // and there's a difference in statistics: our accumulate gas used: 10000000, should be 0.
-    "traces/1757423902/00000152.json",
     "traces/1757423433/00000024.json", // note [seko] The block should be rejected, yet we imported it.
     // note [seko] log message:
     // LOG   [accumulate] Code with hash 0xbaf736ff7927f6f7dfa744a10a67a48b261ae89bbe5c712f7c1f0ee023776661 not found for service 1467575786.
@@ -25,10 +21,6 @@ main(runners, process.argv.slice(2), "test-vectors/jam-conformance/fuzz-reports/
     "traces/1757406516/00000021.json", // note [seko] storage difference, similar to traces/1757092821/00000156.json
     "traces/1757406516/00000022.json", // note [seko] storage difference, similar to traces/1757092821/00000156.json
     "traces/1757406441/00000116.json", // note [seko] storage differences, different accumulate gas used, extra item in accumulation output log
-    // note [seko] log message:
-    // LOG   [accumulate] Code with hash 0xbaf736ff7927f6f7dfa744a10a67a48b261ae89bbe5c712f7c1f0ee023776661 not found for service 306895876.
-    // accumulate gas used should be 0 but isn't
-    "traces/1757406356/00000019.json",
     "genesis.json",
   ],
 })
