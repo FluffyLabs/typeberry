f7e61b8e8fb3e77c25c9cf22da79cd62951ffc4b
Mateusz Sikora <ms1qaz@gmail.com>
2025-09-16 12:33:24 +0200

    review fixes

--- packages/jam/transition/accumulate/accumulate-output.ts
+++ packages/jam/transition/accumulate/accumulate-output.ts
@@ -1,6 +1,8 @@
 import { BytesBlob } from "@typeberry/bytes";
+import { SortedArray } from "@typeberry/collections";
 import { KeccakHasher } from "@typeberry/hash/keccak.js";
 import { u32AsLeBytes } from "@typeberry/numbers";
+import { Ordering } from "@typeberry/ordering";
 import type { AccumulationOutput } from "@typeberry/state";
 import { binaryMerkleization } from "@typeberry/state-merkleization";
 import { getKeccakTrieHasher } from "@typeberry/trie/hasher.js";
@@ -12,7 +14,8 @@ type AccumulateRootInput = {
 
 export class AccumulateOutput {
   async transition({ accumulationOutputLog }: AccumulateRootInput): Promise<AccumulateRoot> {
-    const rootHash = await getRootHash(accumulationOutputLog);
+    const yieldedRootsSortedByServiceId = SortedArray.fromArray(accumulationOutputComparator, accumulationOutputLog);
+    const rootHash = await getRootHash(yieldedRootsSortedByServiceId);
     return rootHash;
   }
 }
@@ -22,13 +25,26 @@ export class AccumulateOutput {
  *
  * https://graypaper.fluffylabs.dev/#/38c4e62/3c9d013c9d01?v=0.7.0
  */
-async function getRootHash(yieldedRoots: AccumulationOutput[]): Promise<AccumulateRoot> {
+async function getRootHash(yieldedRoots: SortedArray<AccumulationOutput>): Promise<AccumulateRoot> {
   const keccakHasher = await KeccakHasher.create();
   const trieHasher = getKeccakTrieHasher(keccakHasher);
-  const yieldedRootsSortedByServiceId = yieldedRoots.sort((a, b) => a.serviceId - b.serviceId);
-  const values = yieldedRootsSortedByServiceId.map(({ serviceId, output }) => {
+  const values = yieldedRoots.array.map(({ serviceId, output }) => {
     return BytesBlob.blobFromParts([u32AsLeBytes(serviceId), output.raw]);
   });
 
   return binaryMerkleization(values, trieHasher);
+}
+
+function accumulationOutputComparator(a: AccumulationOutput, b: AccumulationOutput) {
+  const result = a.serviceId - b.serviceId;
+
+  if (result < 0) {
+    return Ordering.Less;
+  }
+
+  if (result > 0) {
+    return Ordering.Greater;
+  }
+
+  return Ordering.Equal;
 }
