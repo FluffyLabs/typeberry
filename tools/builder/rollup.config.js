a8264fae75d4391af10ce7fa5791a9a78e0bdcf7
Tomasz Drwięga <tomusdrw@gmail.com>
2025-09-18 07:40:16 +0200

    Build for both CJS and ESM.

--- tools/builder/rollup.config.js
+++ tools/builder/rollup.config.js
@@ -6,22 +6,35 @@ import typescript from "rollup-plugin-typescript2";
 
 import setup from "./setup.cjs";
 
+const baseBuild = {
+  input: "./pkg.ts",
+  plugins: [resolve(), commonjs() /* only for blake2b? */, typescript(), json()],
+  treeshake: {
+    moduleSideEffects: false,
+  },
+  external: ["lmdb", "@matrixai/quic", "node:fs", "node:assert", "node:os"],
+};
+
 export default [
+  // ES Module build
   {
-    input: "./pkg.ts",
+    ...baseBuild,
     output: {
-      file: setup.outFile,
+      file: setup.cjsOutFile,
       format: "cjs",
     },
-    plugins: [resolve(), commonjs() /* only for blake2b? */, typescript(), json()],
-    treeshake: {
-      moduleSideEffects: false,
+  },
+  // Common JS build
+  {
+    ...baseBuild,
+    output: {
+      file: setup.esmOutFile,
+      format: "es",
     },
-    external: ["lmdb", "@matrixai/quic", "node:fs", "node:assert", "node:os"],
   },
   {
     input: setup.typesInput,
-    output: [{ file: setup.outFile.replace(".js", ".d.ts"), format: "es" }],
+    output: [{ file: setup.esmOutFile.replace(".js", ".d.ts"), format: "es" }],
     plugins: [
       dts({
         respectExternal: true,
