e085a640d71494ba3750f492528e7628006e4c7d
Tomasz Drwięga <tomusdrw@gmail.com>
2025-09-25 23:03:09 +0200

    Fix npm publish again.

--- tools/build-for-npm.sh
+++ tools/build-for-npm.sh
@@ -20,36 +20,53 @@ $BUILD ./bin/jam/index.ts -o $DIST_FOLDER
 #
 # Despite using `-a` flag, @vercel/ncc does not bundle the worker files,
 # so they still point to some external files via `import` statements.
-# To fix that, we manually build workers and create symlinks.
-
-cd $DIST_FOLDER
-rm ./bootstrap-importer.mjs && ln -s ./importer/index.js bootstrap-importer.mjs
-rm ./bootstrap-network.mjs && ln -s ./jam-network/index.js bootstrap-network.mjs
-rm ./bootstrap-generator.mjs && ln -s ./block-generator/index.js bootstrap-generator.mjs
-cd -
+# To fix that, we manually build workers and move the files inside.
 
 # Build all workers separately and then the main binary
 $BUILD ./workers/importer/index.ts -o $DIST_FOLDER/importer
 $BUILD ./workers/jam-network/index.ts -o $DIST_FOLDER/jam-network
 $BUILD ./workers/block-generator/index.ts -o $DIST_FOLDER/block-generator
 
-# copy worker wasm files
-cp $DIST_FOLDER/**/*.wasm $DIST_FOLDER/ || true # ignore overwrite errors
+# copy some files that should be there
 cp ./LICENSE $DIST_FOLDER/
 cp ./README.md $DIST_FOLDER/
 
-# Make index.js executable and insert shebang
-echo '#!/usr/bin/env node' > $DIST_FOLDER/temp.js && cat $DIST_FOLDER/index.js >> $DIST_FOLDER/temp.js && mv $DIST_FOLDER/temp.js $DIST_FOLDER/index.js
-chmod +x $DIST_FOLDER/index.js
-
 VERSION=$(node -p "require('./package.json').version")
+
+# Flatten the workers structure
+cd $DIST_FOLDER
+
+cd ./importer
+rm *.mjs || true
+mv index.js bootstrap-importer.mjs
+mv index.js.map bootstrap-importer.mjs.map
+mv * ../
+cd ../jam-network
+rm *.mjs || true
+mv index.js bootstrap-network.mjs
+mv index.js.map bootstrap-network.mjs.map
+mv * ../
+cd ../block-generator
+rm *.mjs || true
+mv index.js bootstrap-generator.mjs
+mv index.js.map bootstrap-generator.mjs.map
+mv * ../
+cd ../
+
+# copy worker wasm files
+cp **/*.wasm ./ || true # ignore overwrite errors
+
+# Make index.js executable and insert shebang
+echo '#!/usr/bin/env node' > ./temp.js && cat ./index.js >> ./temp.js && mv ./temp.js ./index.js
+chmod +x ./index.js
+
 if [ -z "$IS_RELEASE" ]; then
   SHA=$(git rev-parse --short HEAD)
   VERSION="$VERSION-$SHA"
 fi
 
 # build package.json file
-cat > $DIST_FOLDER/package.json << EOF
+cat > ./package.json << EOF
 {
   "name": "@typeberry/jam",
   "version": "$VERSION",
