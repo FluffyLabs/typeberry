d1012ed6c4e25739eaa2b7a22c8df4ca73b55e69
Sebastian Koszuta <sebastian.koszuta@gmail.com>
2025-10-02 15:47:22 +0200

    remove logger dependency from utils

--- packages/core/utils/safe-alloc-uint8array.ts
+++ packages/core/utils/safe-alloc-uint8array.ts
@@ -1,7 +1,3 @@
-import { Logger } from "@typeberry/logger";
-
-const logger = Logger.new(import.meta.filename, "safeAllocUint8Array");
-
 // about 2GB, the maximum ArrayBuffer length on Chrome confirmed by several sources:
 // - https://issues.chromium.org/issues/40055619
 // - https://stackoverflow.com/a/72124984
@@ -10,7 +6,8 @@ export const MAX_LENGTH = 2145386496;
 
 export function safeAllocUint8Array(length: number) {
   if (length > MAX_LENGTH) {
-    logger.warn`Trying to allocate ${length} bytes, which is greater than the maximum of ${MAX_LENGTH}.`;
+    // biome-ignore lint/suspicious/noConsole: can't have a dependency on logger here
+    console.warn(`Trying to allocate ${length} bytes, which is greater than the maximum of ${MAX_LENGTH}.`);
   }
   return new Uint8Array(Math.min(MAX_LENGTH, length));
 }
