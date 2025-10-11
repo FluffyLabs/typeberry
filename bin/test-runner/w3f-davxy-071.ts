ff3d6f192b4965961b66485b55ae385d534d6d27
Tomasz Drwięga <tomusdrw@gmail.com>
2025-10-11 18:16:57 +0200

    Update ignore list after owned privileges

--- bin/test-runner/w3f-davxy-071.ts
+++ bin/test-runner/w3f-davxy-071.ts
@@ -22,24 +22,6 @@ main(runners, process.argv.slice(2), "test-vectors/w3f-davxy_071", {
     "traces/fuzzy/00000143.json",
     "traces/fuzzy/00000163.json",
     "traces/fuzzy/00000185.json",
-    // 0x0d statistics + shitload other
-    "traces/fuzzy/00000058.json",
-    "traces/fuzzy/00000073.json",
-    "traces/fuzzy/00000161.json",
-    "traces/fuzzy/00000179.json",
-    "traces/fuzzy/00000192.json",
-    // 0x03 + shitload of other stuff
-    "traces/fuzzy/00000046.json",
-    "traces/fuzzy/00000061.json",
-    "traces/fuzzy/00000063.json",
-    "traces/fuzzy/00000066.json",
-    "traces/fuzzy/00000076.json",
-    "traces/fuzzy/00000089.json",
-    "traces/fuzzy/00000100.json",
-    "traces/fuzzy/00000107.json",
-    "traces/fuzzy/00000144.json",
-    "traces/fuzzy/00000166.json",
-    "traces/fuzzy/00000184.json",
   ],
 })
   .then((r) => logger.log`${r}`)
