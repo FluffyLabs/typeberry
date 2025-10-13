b6667ce85e6f29e2da39d403cd0cf7bd37942df8
Mateusz Sikora <ms1qaz@gmail.com>
2025-10-13 20:01:39 +0200

    Update W_B value (#718)
    * update W_B value
    
    * update gp link

--- packages/jam/block/gp-constants.ts
+++ packages/jam/block/gp-constants.ts
@@ -1,3 +1,4 @@
+import { Compatibility, GpVersion } from "@typeberry/utils";
 import { MAX_NUMBER_OF_WORK_ITEMS } from "./work-package.js";
 
 /**
@@ -10,7 +11,7 @@ import { MAX_NUMBER_OF_WORK_ITEMS } from "./work-package.js";
  * here are only temporarily for convenience. When we figure out better names
  * and places for these this file will be eradicated.
  *
- * https://graypaper.fluffylabs.dev/#/579bd12/413000413000
+ * https://graypaper.fluffylabs.dev/#/ab2cdbd/442300442300?v=0.7.2
  */
 
 /** `G_I`: The gas allocated to invoke a work-package’s Is-Authorized logic. */
@@ -34,8 +35,8 @@ export const T = 128;
 /** `W_A`: The maximum size of is-authorized code in octets. */
 export const W_A = 64_000;
 
-/** `W_B`: The maximum size of an encoded work-package with extrinsic data and imports. */
-export const W_B = 13_794_305;
+/** `W_B`: The maximum size of the concatenated variable-size blobs, extrinsics and imported segments of a work-package, in octets */
+export const W_B = Compatibility.isGreaterOrEqual(GpVersion.V0_7_2) ? 13_791_360 : 13_794_305;
 
 /** `W_C`: The maximum size of service code in octets. */
 export const W_C = 4_000_000;
