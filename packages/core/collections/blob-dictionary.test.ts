132cb32701e56459c7e38e59af04fbab0413fb88
Mateusz Sikora <ms1qaz@gmail.com>
2025-10-20 17:25:52 +0200

    full compatiblity with HashDictionary

--- packages/core/collections/blob-dictionary.test.ts
+++ packages/core/collections/blob-dictionary.test.ts
@@ -1,13 +1,8 @@
 import assert from "node:assert";
 import { describe, it } from "node:test";
-import { BytesBlob } from "@typeberry/bytes";
-import { BlobDictionary, ListNode } from "./blob-dictionary.js";
-
-// describe("ListNode", () => {
-//     it ("should", () => {
-//         const node = ListNode.new()
-//     });
-// });
+import { Bytes, BytesBlob } from "@typeberry/bytes";
+import { HASH_SIZE } from "@typeberry/hash";
+import { BlobDictionary } from "./blob-dictionary.js";
 
 describe("Blob dictionary", () => {
   it("should add item to BlobDictionary and then return it", () => {
@@ -18,7 +13,7 @@ describe("Blob dictionary", () => {
     assert.strictEqual(dict.has(key), false);
 
     dict.set(key, val);
-
+    console.log("dict after set", JSON.stringify(dict, null, 2));
     assert.strictEqual(dict.has(key), true);
 
     const result = dict.get(key);
@@ -72,5 +67,114 @@ describe("Blob dictionary", () => {
     const result = dict.get(key);
 
     assert.deepStrictEqual(result, val);
+  });
+
+  it("should store a few items with the same prefix and then remove all of them", () => {
+    const entries = [
+      [BytesBlob.parseBlob("0x112233445566778899aabbccddeef1"), { index: 1 }] as const,
+      [BytesBlob.parseBlob("0x112233445566778899aabbccddeef2"), { index: 2 }] as const,
+      [BytesBlob.parseBlob("0x112233445566778899aabbccddeef3"), { index: 3 }] as const,
+      [BytesBlob.parseBlob("0x112233445566778899aabbccddeef4"), { index: 4 }] as const,
+      [BytesBlob.parseBlob("0x112233445566778899aabbccddeef5"), { index: 5 }] as const,
+      [BytesBlob.parseBlob("0x112233445566778899aabbccddeef6"), { index: 6 }] as const,
+      [BytesBlob.parseBlob("0x112233445566778899aabbccddeef7"), { index: 7 }] as const,
+      [BytesBlob.parseBlob("0x112233445566778899aabbccddeef8"), { index: 8 }] as const,
+      [BytesBlob.parseBlob("0x112233445566778899aabbccddeef9"), { index: 9 }] as const,
+    ];
+
+    const dict = BlobDictionary.new(3);
+
+    for (const [key, val] of entries) {
+      dict.set(key, val);
+    }
+
+    for (const [key, val] of entries) {
+      const result = dict.get(key);
+      assert.deepStrictEqual(val, result);
+    }
+
+    for (const [key] of entries) {
+      dict.delete(key);
+    }
+
+    for (const [key] of entries) {
+      assert.strictEqual(dict.has(key), false);
+    }
+  });
+
+  function key(n: number) {
+    return Bytes.fill(HASH_SIZE, n);
+  }
+
+  it("should return true/false for keys present in the dictionary", () => {
+    const dict = BlobDictionary.new();
+    dict.set(key(1), "Hello World!");
+    dict.set(key(2), "Hello!");
+
+    assert.deepStrictEqual(dict.has(key(0)), false);
+    assert.deepStrictEqual(dict.has(key(1)), true);
+    assert.deepStrictEqual(dict.has(key(2)), true);
+    assert.deepStrictEqual(dict.has(key(3)), false);
+  });
+
+  it("should set and get some values", () => {
+    const dict = BlobDictionary.new();
+    dict.set(key(1), "Hello World!");
+    dict.set(key(2), "Hello!");
+
+    assert.deepStrictEqual(dict.get(key(0)), undefined);
+    assert.deepStrictEqual(dict.get(key(1)), "Hello World!");
+    assert.deepStrictEqual(dict.get(key(2)), "Hello!");
+    assert.deepStrictEqual(dict.get(key(4)), undefined);
+  });
+
+  it("should remove some values", () => {
+    const dict = BlobDictionary.new();
+    dict.set(key(1), "Hello World!");
+    dict.set(key(2), "Hello!");
+    assert.deepStrictEqual(dict.has(key(1)), true);
+    assert.deepStrictEqual(dict.has(key(2)), true);
+
+    dict.delete(key(0));
+    dict.delete(key(1));
+    dict.delete(key(3));
+
+    assert.deepStrictEqual(dict.has(key(0)), false);
+    assert.deepStrictEqual(dict.has(key(1)), false);
+    assert.deepStrictEqual(dict.has(key(2)), true);
+    assert.deepStrictEqual(dict.has(key(3)), false);
+  });
+
+  it("should iterate over values", () => {
+    const dict = BlobDictionary.new();
+    dict.set(key(1), "Hello World!");
+    dict.set(key(2), "Hello!");
+
+    const values = Array.from(dict.values());
+
+    assert.deepStrictEqual(values, ["Hello World!", "Hello!"]);
+  });
+
+  it("should iterate over keys", () => {
+    const dict = BlobDictionary.new();
+    dict.set(key(1), "Hello World!");
+    dict.set(key(2), "Hello!");
+
+    const keys = Array.from(dict.keys());
+
+    assert.deepStrictEqual(keys, [key(1), key(2)]);
+  });
+
+  it("should iterate over entries", () => {
+    const dict = BlobDictionary.new();
+    dict.set(key(1), "Hello World!");
+    dict.set(key(2), "Hello!");
+
+    const values = Array.from(dict);
+
+    assert.deepStrictEqual(values, [
+      [key(1), "Hello World!"],
+      [key(2), "Hello!"],
+    ]);
   });
 });
--- packages/core/collections/blob-dictionary.ts
+++ packages/core/collections/blob-dictionary.ts
@@ -1,11 +1,9 @@
 import { BytesBlob } from "@typeberry/bytes";
-import { assertNever, check } from "@typeberry/utils";
+import { assertNever, check, WithDebug } from "@typeberry/utils";
 
 const CHUNK_SIZE = 6;
 
-export interface Keyable extends BytesBlob {
-  // chunks(chunkLength: number): Generator<BytesBlob, void, void>;
-}
+export interface Keyable extends BytesBlob {}
 
 export function bytesAsU48(bytes: Uint8Array): number {
   const len = bytes.length;
@@ -21,12 +19,16 @@ export function bytesAsU48(bytes: Uint8Array): number {
   return value * 8 + len;
 }
 
-
 class MapNode<V> {
+  key?: Keyable | undefined;
   value?: V | undefined;
   children: Map<number, MapNode<V> | ListNode<V>> = new Map();
 
   private constructor() {}
+
+  static new<V>(): MapNode<V> {
+    return new MapNode<V>();
+  }
 
   static fromListNode<T>(node: ListNode<T>): MapNode<T> {
     const mapNode = new MapNode<T>();
@@ -34,8 +36,8 @@ class MapNode<V> {
     for (const [key, value] of node.children) {
       const currentKey = BytesBlob.blobFrom(key.raw.subarray(0, CHUNK_SIZE));
       const subKey = BytesBlob.blobFrom(key.raw.subarray(CHUNK_SIZE));
-      const child = mapNode.getChild(currentKey) as ListNode<T> ?? ListNode.new<T>();
-      
+      const child = (mapNode.getChild(currentKey) as ListNode<T>) ?? ListNode.new<T>();
+
       if (subKey.length > 0) {
         child.pushOrReplace(subKey, value);
       } else {
@@ -55,30 +57,71 @@ class MapNode<V> {
 
   setChild(keyChunk: Keyable, node: MapNode<V> | ListNode<V>) {
     const chunkAsNumber = bytesAsU48(keyChunk.raw);
-    return this.children.set(chunkAsNumber, node);
+    this.children.set(chunkAsNumber, node);
+  }
+
+  setValue(key: Keyable, value: V) {
+    this.key = key;
+    if (this.value !== undefined && value === undefined) {
+      this.value = value;
+      return OperationKind.Remove;
+    }
+    if (this.value === undefined && value !== undefined) {
+      this.value = value;
+      return OperationKind.Add;
+    }
+
+    this.value = value;
+    return OperationKind.Override;
   }
 }
 
+enum OperationKind {
+  Remove = -1,
+  Override = 0,
+  Add = 1,
+}
 export class ListNode<V> {
   value?: V | undefined;
+  key?: Keyable | undefined;
   children: [Keyable, V][] = [];
 
   private constructor() {}
 
   find(key: Keyable) {
-    const result = this.children.find(item => item[0].isEqualTo(key));
+    const result = this.children.find((item) => item[0].isEqualTo(key));
     if (result !== undefined) {
       return result[1];
     }
   }
 
-  pushOrReplace(key: Keyable, value: V) {
-    const existing = this.children.find(item => item[0].isEqualTo(key));
+  pushOrReplace(key: Keyable, value: V): OperationKind {
+    const existing = this.children.find((item) => item[0].isEqualTo(key));
     if (existing !== undefined) {
       existing[1] = value;
-    } else {
-      this.children.push([key, value]);
+      if (existing[1] !== undefined && value === undefined) {
+        return OperationKind.Remove;
+      }
+
+      return OperationKind.Override;
     }
+    this.children.push([key, value]);
+    return OperationKind.Add;
+  }
+
+  setValue(key: Keyable, value: V) {
+    this.key = key;
+    if (this.value !== undefined && value === undefined) {
+      this.value = value;
+      return OperationKind.Remove;
+    }
+    if (this.value === undefined && value !== undefined) {
+      this.value = value;
+      return OperationKind.Add;
+    }
+
+    this.value = value;
+    return OperationKind.Override;
   }
 
   static new<V>() {
@@ -89,62 +132,73 @@ export class ListNode<V> {
 type Node<V> = ListNode<V> | MapNode<V>;
 type MaybeNode<V> = Node<V> | undefined;
 
-export class BlobDictionary<K extends Keyable, V> {
-  private root: Node<V> = ListNode.new<V>();
-  private constructor(private mapNodeThreshold: number) {}
+export class BlobDictionary<K extends Keyable, V> extends WithDebug {
+  private root: Node<V> = MapNode.new<V>();
+  private keyvals: Map<Keyable, V> = new Map();
 
-  static new<K extends Keyable, V>(mapNodeThreshold: number = 0) {
-    return new BlobDictionary<K, V>(mapNodeThreshold);
+  private constructor(private mapNodeThreshold: number) {
+    super();
   }
 
-  private asNumbers(key: K): number[] {
-    const numbers: number[] = [];
-    const chunks = Array.from(key.chunks(CHUNK_SIZE));
-    const chunksLength = chunks.length;
+  get size(): number {
+    return this.keyvals.size;
+  }
 
-    for (let i = 0; i < chunksLength; i += 1) {
-      const num = bytesAsU48(chunks[i].raw);
-      numbers.push(num);
-    }
-
-    return numbers;
+  static new<K extends Keyable, V>(mapNodeThreshold = 0) {
+    return new BlobDictionary<K, V>(mapNodeThreshold);
   }
 
   set(key: K, value: V): void {
     let node: MaybeNode<V> = this.root;
-    const path = this.asNumbers(key);
     const keyChunkGenerator = key.chunks(CHUNK_SIZE);
     let depth = 0;
-    while(node instanceof MapNode) {
+
+    while (node instanceof MapNode) {
       const keyChunk = keyChunkGenerator.next().value;
       if (keyChunk === undefined) {
         break;
       }
 
+      depth += 1;
       const maybeNode = node.getChild(keyChunk);
+
       if (maybeNode instanceof MapNode) {
         node = maybeNode;
       } else if (maybeNode instanceof ListNode) {
         if (maybeNode.children.length >= this.mapNodeThreshold) {
           const mapNode: MapNode<V> = MapNode.fromListNode(maybeNode);
-          node.setChild(keyChunk, mapNode)
+          node.setChild(keyChunk, mapNode);
           node = mapNode;
         } else {
           node = maybeNode;
         }
       } else {
         const newNode = ListNode.new<V>();
-        const keyAsNumber = bytesAsU48(keyChunk.raw);
-        node.children.set(keyAsNumber, newNode);
-        break;
+        node.setChild(keyChunk, newNode);
+        node = newNode;
       }
-
-      depth += 1;
     }
 
-    const listNode = node as ListNode<V>; // it has to be list node here
-    const subkey = key.raw.subarray(CHUNK_SIZE * depth);
-    listNode.pushOrReplace(BytesBlob.blobFrom(subkey), value);
+    if (node instanceof MapNode) {
+      const operation = node.setValue(key, value);
+      this.updateKeyVals(operation, node.key ?? key, value);
+    } else if (node instanceof ListNode) {
+      const subkey = BytesBlob.blobFrom(key.raw.subarray(CHUNK_SIZE * depth));
+      const operation = subkey.length > 0 ? node.pushOrReplace(subkey, value) : node.setValue(key, value);
+      this.updateKeyVals(operation, node.key ?? key, value);
+    } else {
+      assertNever(node);
+    }
+  }
+
+  private updateKeyVals(operation: OperationKind, key: Keyable, value: V) {
+    if (operation === OperationKind.Add) {
+      this.keyvals.set(key, value);
+    } else if (operation === OperationKind.Override) {
+      this.keyvals.set(key, value);
+    } else {
+      this.keyvals.delete(key);
+    }
   }
 
   get(key: K): V | undefined {
@@ -157,17 +211,24 @@ export class BlobDictionary<K extends Keyable, V> {
       if (pathChunk === undefined) {
         break;
       }
-      node = node.getChild(pathChunk)
+      node = node.getChild(pathChunk);
       depth += 1;
     }
 
     if (node instanceof MapNode) {
       return node.value;
-    } else if (node instanceof ListNode) {
-      const subkey = key.raw.subarray(depth * CHUNK_SIZE);
-      return node.find(BytesBlob.blobFrom(subkey));
-    } 
-    assertNever(node as never);
+    }
+
+    if (node instanceof ListNode) {
+      const keyOffset = depth * CHUNK_SIZE;
+      if (keyOffset >= key.length) {
+        return node.value;
+      }
+      const subkey = BytesBlob.blobFrom(key.raw.subarray(depth * CHUNK_SIZE));
+      return node.find(subkey);
+    }
+
+    return undefined;
   }
 
   has(key: K): boolean {
@@ -176,5 +237,26 @@ export class BlobDictionary<K extends Keyable, V> {
 
   delete(key: K): void {
     this.set(key, undefined as V);
+  }
+
+  keys() {
+    return this.keyvals.keys();
+  }
+
+  values() {
+    return this.keyvals.values();
+  }
+
+  [Symbol.iterator]() {
+    return this.keyvals.entries();
+  }
+
+  /** Create a new hash dictionary from given entires array. */
+  static fromEntries<K extends Keyable, V>(entries: [K, V][]): BlobDictionary<K, V> {
+    const dict = BlobDictionary.new<K, V>();
+    for (const [key, value] of entries) {
+      dict.set(key, value);
+    }
+    return dict;
   }
 }
