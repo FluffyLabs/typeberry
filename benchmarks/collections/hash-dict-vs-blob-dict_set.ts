2393348e725908766d78f0ff854f9ff414d06615
Mateusz Sikora <ms1qaz@gmail.com>
2025-10-24 22:03:55 +0200

    more benchmarks

--- benchmarks/collections/hash-dict-vs-blob-dict_set.ts
+++ benchmarks/collections/hash-dict-vs-blob-dict_set.ts
@@ -4,47 +4,95 @@ import { Bytes } from "@typeberry/bytes";
 import { StringHashDictionary } from "@typeberry/collections";
 import { BlobDictionary } from "@typeberry/collections/blob-dictionary.js";
 import { HASH_SIZE, type OpaqueHash } from "@typeberry/hash";
+import blake2b from "blake2b";
 
 const NO_OF_KEYS = 1000;
 
-function key(n: number) {
+function blake2bHasher(bytes: Bytes<HASH_SIZE>) {
+  const hasher = blake2b(HASH_SIZE);
+  return hasher.update(bytes.raw).digest("binary");
+}
+function longCollisionKey(n: number) {
   const key = Bytes.blobFromString(`${n}`);
   const ret = Bytes.zero(HASH_SIZE);
   ret.raw.set(key.raw, 0);
+  ret.raw.reverse();
   return ret;
 }
 
-const KEYS = Array.from({ length: NO_OF_KEYS }, (_, i) => key(i));
+function hashKey(n: number) {
+  return Bytes.fromBlob(blake2bHasher(longCollisionKey(n)), HASH_SIZE);
+}
+
+const LONG_COLLISION_KEYS = Array.from({ length: NO_OF_KEYS }, (_, i) => longCollisionKey(i));
+const HASH_KEYS = Array.from({ length: NO_OF_KEYS }, (_, i) => hashKey(i));
 
 export default function run() {
-  return suite(
-    "Comparing set operation in two hash dicts",
+  const promises: ReturnType<typeof suite>[] = [];
 
-    add("HashDictionary", () => {
-      const map = StringHashDictionary.new<OpaqueHash, number>();
+  for (let threshold = 0; threshold < 10; threshold++) {
+    const longCollisionTestPromise = suite(
+      `Comparing set operation in two hash dicts using long collition keys and BlobDictionary(${threshold})`,
 
-      return () => {
-        for (let k = 0; k < NO_OF_KEYS; k += 1) {
-          map.set(KEYS[k], k);
-        }
-      };
-    }),
+      add("StringHashDictionary", () => {
+        const map = StringHashDictionary.new<OpaqueHash, number>();
 
-    add("BlobDictionary", () => {
-      const map = BlobDictionary.new<OpaqueHash, number>(7);
+        return () => {
+          for (let k = 0; k < NO_OF_KEYS; k += 1) {
+            map.set(LONG_COLLISION_KEYS[k], k);
+          }
+        };
+      }),
 
-      return () => {
-        for (let k = 0; k < NO_OF_KEYS; k += 1) {
-          map.set(KEYS[k], k);
-        }
-      };
-    }),
+      add("BlobDictionary", () => {
+        const map = BlobDictionary.new<OpaqueHash, number>(threshold);
 
-    cycle(),
-    complete(),
-    configure({}),
-    ...save(import.meta.filename),
-  );
+        return () => {
+          for (let k = 0; k < NO_OF_KEYS; k += 1) {
+            map.set(LONG_COLLISION_KEYS[k], k);
+          }
+        };
+      }),
+
+      cycle(),
+      complete(),
+      configure({}),
+      ...save(import.meta.filename),
+    );
+
+    const hashKeyTestPromise = suite(
+      `Comparing set operation in two hash dicts using hash keys and BlobDictionary(${threshold})`,
+
+      add("StringHashDictionary", () => {
+        const map = StringHashDictionary.new<OpaqueHash, number>();
+
+        return () => {
+          for (let k = 0; k < NO_OF_KEYS; k += 1) {
+            map.set(HASH_KEYS[k], k);
+          }
+        };
+      }),
+
+      add("BlobDictionary", () => {
+        const map = BlobDictionary.new<OpaqueHash, number>(threshold);
+
+        return () => {
+          for (let k = 0; k < NO_OF_KEYS; k += 1) {
+            map.set(HASH_KEYS[k], k);
+          }
+        };
+      }),
+
+      cycle(),
+      complete(),
+      configure({}),
+      ...save(import.meta.filename),
+    );
+
+    promises.push(longCollisionTestPromise, hashKeyTestPromise);
+  }
+
+  return Promise.allSettled(promises);
 }
 
 if (import.meta.url === pathToFileURL(process.argv[1]).href) {
