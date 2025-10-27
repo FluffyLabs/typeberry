d5f69995a7e932e3c7d4831ad90cc9af969db6a5
HeyImStas <beebx4f@protonmail.com>
2025-10-27 10:34:42 +0100

    moved declaration of ALL_VERSIONS_IN_ORDER before it's use in parseCurrentVersion

--- packages/core/utils/compatibility.ts
+++ packages/core/utils/compatibility.ts
@@ -10,14 +10,14 @@ export enum TestSuite {
   JAMDUNA = "jamduna",
 }
 
+const ALL_VERSIONS_IN_ORDER = [GpVersion.V0_6_7, GpVersion.V0_7_0, GpVersion.V0_7_1, GpVersion.V0_7_2];
+
 export const DEFAULT_SUITE = TestSuite.W3F_DAVXY;
 export const DEFAULT_VERSION = GpVersion.V0_7_2;
 
 const env = typeof process === "undefined" ? {} : process.env;
 export let CURRENT_VERSION = parseCurrentVersion(env.GP_VERSION) ?? DEFAULT_VERSION;
 export let CURRENT_SUITE = parseCurrentSuite(env.TEST_SUITE) ?? DEFAULT_SUITE;
-
-const ALL_VERSIONS_IN_ORDER = [GpVersion.V0_6_7, GpVersion.V0_7_0, GpVersion.V0_7_1, GpVersion.V0_7_2];
 
 function parseCurrentVersion(env?: string): GpVersion | undefined {
   if (env === undefined) {
