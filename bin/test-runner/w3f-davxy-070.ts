49f4660315e1142f49deee083dae6a6f8bc00858
Mateusz Sikora <ms1qaz@gmail.com>
2025-11-06 14:51:38 +0100

    add remaining tests to ignore as they require a bug from GP 0.7.0 to pass them

--- bin/test-runner/w3f-davxy-070.ts
+++ bin/test-runner/w3f-davxy-070.ts
@@ -6,7 +6,11 @@ main(runners, process.argv.slice(2), "test-vectors/w3f-davxy_070", {
   accepted: {
     ".json": ["traces"],
   },
-  ignored: ["genesis.json"],
+  ignored: [
+    "genesis.json",
+    "1758622442/00000164.json", // bug in GP 070
+    "1758622403/00000239.json", // bug in GP 070
+  ],
 })
   .then((r) => logger.log`${r}`)
   .catch((e) => {
