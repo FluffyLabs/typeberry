0a3acb238f7e5f27d79ef11c71436fa054663cc4
Mateusz Sikora <ms1qaz@gmail.com>
2025-11-18 20:21:34 +0100

    BlobDictionary (#732)
    * wip
    
    * add BlobDictionary collection
    
    * full compatiblity with HashDictionary
    
    * add benchmarks
    
    * bug fixes
    
    * fix part of build errors
    
    * fix part of build errors vol.2
    
    * refactor
    
    * add docs and tests
    
    * fix benchmarks
    
    * fix circular deps
    
    * simplify TruncatedHashDictionary impementation
    
    * cleaning
    
    * fix tests
    
    * add better docs
    
    * uncomment tests
    
    * better implementation of bytesAsU48
    
    * fix tests
    
    * more benchmarks
    
    * review fixes
    
    * more benchmarks and review fixes
    
    * simpler benchmarks and more review fixes
    
    * fix typo
    
    * fix CI workflow
    
    * fix CI workflow vol.2
    
    * review fixes
    
    * review fixes

---
+++ benchmarks/collections/hash-dict-vs-blob-dict_delete.ts
@@ -0,0 +1,123 @@
+import { pathToFileURL } from "node:url";
+import { add, complete, configure, cycle, save, suite } from "@typeberry/benchmark/setup.js";
+import { Bytes } from "@typeberry/bytes";
+import { StringHashDictionary } from "@typeberry/collections";
+import { BlobDictionary } from "@typeberry/collections/blob-dictionary.js";
+import { HASH_SIZE, type OpaqueHash } from "@typeberry/hash";
+import blake2b from "blake2b";
+
+const NO_OF_KEYS = 1000;
+
+function blake2bHasher(bytes: Bytes<HASH_SIZE>) {
+  const hasher = blake2b(HASH_SIZE);
+  return hasher.update(bytes.raw).digest("binary");
+}
+function longCollisionKey(n: number) {
+  const key = Bytes.blobFromString(`${n}`);
+  const ret = Bytes.zero(HASH_SIZE);
+  ret.raw.set(key.raw, 0);
+  ret.raw.reverse();
+  return ret;
+}
+
+function hashKey(n: number) {
+  return Bytes.fromBlob(blake2bHasher(longCollisionKey(n)), HASH_SIZE);
+}
+
+const LONG_COLLISION_KEYS = Array.from({ length: NO_OF_KEYS }, (_, i) => longCollisionKey(i));
+const HASH_KEYS = Array.from({ length: NO_OF_KEYS }, (_, i) => hashKey(i));
+const MIN_THRESHOLD = 1;
+const MAX_THRESHOLD = 6;
+
+export default function run() {
+  const promises: ReturnType<typeof suite>[] = [];
+
+  const longCollisionKeysBlobDictBenchmarks: ReturnType<typeof add>[] = [];
+
+  for (let threshold = MIN_THRESHOLD; threshold < MAX_THRESHOLD; threshold++) {
+    longCollisionKeysBlobDictBenchmarks.push(
+      add(`BlobDictionary(${threshold})`, () => {
+        const map = BlobDictionary.new<OpaqueHash, number>(threshold);
+        for (let k = 0; k < NO_OF_KEYS; k += 1) {
+          map.set(LONG_COLLISION_KEYS[k], k);
+        }
+        return () => {
+          for (let k = 0; k < NO_OF_KEYS; k += 1) {
+            map.delete(LONG_COLLISION_KEYS[k]);
+          }
+        };
+      }),
+    );
+  }
+  const longCollisionTestPromise = suite(
+    `Comparing delete operation in two hash dicts using long collision keys and BlobDictionary(n: [${MIN_THRESHOLD}: ${MAX_THRESHOLD}))`,
+
+    add("StringHashDictionary", () => {
+      const map = StringHashDictionary.new<OpaqueHash, number>();
+      for (let k = 0; k < NO_OF_KEYS; k += 1) {
+        map.set(LONG_COLLISION_KEYS[k], k);
+      }
+      return () => {
+        for (let k = 0; k < NO_OF_KEYS; k += 1) {
+          map.delete(LONG_COLLISION_KEYS[k]);
+        }
+      };
+    }),
+
+    ...longCollisionKeysBlobDictBenchmarks,
+
+    cycle(),
+    complete(),
+    configure({}),
+    ...save(import.meta.filename),
+  );
+
+  const hashKeysBlobDictBenchmarks: ReturnType<typeof add>[] = [];
+
+  for (let threshold = MIN_THRESHOLD; threshold < MAX_THRESHOLD; threshold++) {
+    hashKeysBlobDictBenchmarks.push(
+      add(`BlobDictionary(${threshold})`, () => {
+        const map = BlobDictionary.new<OpaqueHash, number>(threshold);
+        for (let k = 0; k < NO_OF_KEYS; k += 1) {
+          map.set(HASH_KEYS[k], k);
+        }
+        return () => {
+          for (let k = 0; k < NO_OF_KEYS; k += 1) {
+            map.delete(HASH_KEYS[k]);
+          }
+        };
+      }),
+    );
+  }
+
+  const hashKeyTestPromise = suite(
+    `Comparing delete operation in two hash dicts using hash keys and BlobDictionary(n: [${MIN_THRESHOLD}: ${MAX_THRESHOLD}))`,
+
+    add("StringHashDictionary", () => {
+      const map = StringHashDictionary.new<OpaqueHash, number>();
+      for (let k = 0; k < NO_OF_KEYS; k += 1) {
+        map.set(HASH_KEYS[k], k);
+      }
+      return () => {
+        for (let k = 0; k < NO_OF_KEYS; k += 1) {
+          map.delete(HASH_KEYS[k]);
+        }
+      };
+    }),
+
+    ...hashKeysBlobDictBenchmarks,
+
+    cycle(),
+    complete(),
+    configure({}),
+    ...save(import.meta.filename),
+  );
+
+  promises.push(longCollisionTestPromise, hashKeyTestPromise);
+
+  return Promise.allSettled(promises);
+}
+
+if (import.meta.url === pathToFileURL(process.argv[1]).href) {
+  run();
+}
