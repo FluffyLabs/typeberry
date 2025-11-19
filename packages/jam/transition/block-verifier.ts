f776ccefa4178ad6adc0f9275f6c0b269aeb454d
Tomek Drwięga <tomusdrw@users.noreply.github.com>
2025-11-19 11:42:09 +0100

    Check extrinsic hash in state vectors. (#787)

--- packages/jam/transition/block-verifier.ts
+++ packages/jam/transition/block-verifier.ts
@@ -16,13 +16,21 @@ export enum BlockVerifierError {
 
 const ZERO_HASH: HeaderHash = Bytes.zero(HASH_SIZE).asOpaque();
 
+type BlockVerificationOptions = {
+  /** Skip verification of parent (for state-transition vectors) */
+  skipParentAndStateRoot: boolean;
+};
+
 export class BlockVerifier {
   constructor(
     public readonly hasher: TransitionHasher,
     private readonly blocks: BlocksDb,
   ) {}
 
-  async verifyBlock(block: BlockView): Promise<Result<HeaderHash, BlockVerifierError>> {
+  async verifyBlock(
+    block: BlockView,
+    options: BlockVerificationOptions = { skipParentAndStateRoot: false },
+  ): Promise<Result<HeaderHash, BlockVerifierError>> {
     const headerView = block.header.view();
     const headerHash = this.hasher.header(headerView);
     // check if current block is already imported
@@ -35,7 +43,7 @@ export class BlockVerifier {
     // https://graypaper.fluffylabs.dev/#/cc517d7/0c9d000c9d00?v=0.6.5
     const parentHash = headerView.parentHeaderHash.materialize();
     // importing genesis block
-    if (!parentHash.isEqualTo(ZERO_HASH)) {
+    if (!parentHash.isEqualTo(ZERO_HASH) && !options.skipParentAndStateRoot) {
       const parentBlock = this.blocks.getHeader(parentHash);
       if (parentBlock === null) {
         return Result.error(BlockVerifierError.ParentNotFound, () => `Parent ${parentHash.toString()} not found`);
@@ -65,27 +73,25 @@ export class BlockVerifier {
       );
     }
 
-    // Check if the state root is valid.
-    // https://graypaper.fluffylabs.dev/#/cc517d7/0c18010c1801?v=0.6.5
-    const stateRoot = headerView.priorStateRoot.materialize();
-    const posteriorStateRoot = this.blocks.getPostStateRoot(parentHash);
-    if (posteriorStateRoot === null) {
-      return Result.error(
-        BlockVerifierError.StateRootNotFound,
-        () => `Posterior state root ${parentHash.toString()} not found`,
-      );
-    }
-    if (!stateRoot.isEqualTo(posteriorStateRoot)) {
-      return Result.error(
-        BlockVerifierError.InvalidStateRoot,
-        () => `Invalid prior state root: ${stateRoot.toString()}, expected ${posteriorStateRoot.toString()} (ours)`,
-      );
+    if (!options.skipParentAndStateRoot) {
+      // Check if the state root is valid.
+      // https://graypaper.fluffylabs.dev/#/ab2cdbd/0c73010c7301?v=0.7.2
+      const stateRoot = headerView.priorStateRoot.materialize();
+      const posteriorStateRoot = this.blocks.getPostStateRoot(parentHash);
+      if (posteriorStateRoot === null) {
+        return Result.error(
+          BlockVerifierError.StateRootNotFound,
+          () => `Posterior state root ${parentHash.toString()} not found`,
+        );
+      }
+      if (!stateRoot.isEqualTo(posteriorStateRoot)) {
+        return Result.error(
+          BlockVerifierError.InvalidStateRoot,
+          () => `Invalid prior state root: ${stateRoot.toString()}, expected ${posteriorStateRoot.toString()} (ours)`,
+        );
+      }
     }
 
     return Result.ok(headerHash.hash);
-  }
-
-  hashHeader(block: BlockView) {
-    return this.hasher.header(block.header.view());
   }
 }
