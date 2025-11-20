8408216101d8c05383331b957642985b1799c897
Tomek Drwięga <tomusdrw@users.noreply.github.com>
2025-11-20 09:33:48 +0100

    Update conformance vectors (#796)
    * update ignore list
    
    * update vectors ref

--- bin/test-runner/jam-conformance-071.ts
+++ bin/test-runner/jam-conformance-071.ts
@@ -10,11 +10,14 @@ main(runners, process.argv.slice(2), "test-vectors/jam-conformance/fuzz-reports/
     // unrecognized test cases
     // no timeslot in pre-state - valid behavior
     "1763371531/00000042.json",
+    "1763489287/00000872.json",
     // WorkItemsCount === 0 - valid behavior
     "1763371098/00000006.json",
     "1763372314/00000094.json",
     // Invalid ticket attempt - valid behavior
     "1763371155/00000055.json",
+    "1763488328/00000051.json",
+    "1763487981/00000051.json",
   ],
 })
   .then((r) => logger.log`${r}`)
