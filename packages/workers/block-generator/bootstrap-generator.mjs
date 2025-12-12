cc06594f93f806140729ab078fb9d428a34e23c9
Mateusz Sikora <ms1qaz@gmail.com>
2025-12-12 13:19:51 +0100

    remove block generator

--- packages/workers/block-generator/bootstrap-generator.mjs
+++
@@ -1,3 +0,0 @@
-import { tsImport } from "tsx/esm/api";
-
-await tsImport("./bootstrap-main.ts", import.meta.url);
--- packages/workers/block-generator/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./main.js";
-export * from "./metrics.js";
-export * from "./protocol.js";
-export const WORKER = new URL(import.meta.resolve("./bootstrap-generator.mjs"));
