05ec3bf8f86d5e52c33649dd4b09390b6641559e
Mateusz Sikora <ms1qaz@gmail.com>
2025-12-16 16:46:29 +0100

    remove todo comment

--- packages/jam/block/header.ts
+++ packages/jam/block/header.ts
@@ -127,8 +127,6 @@ export class Header extends WithDebug {
     return Object.assign(Header.empty(), h);
   }
 
-  // TODO [ToDr] the fields should be readonly
-
   /**
    * `H_p`: Hash of the parent header.
    *
