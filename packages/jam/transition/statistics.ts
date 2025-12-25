ab81f41c5a2c9395ae47e7910be19991a87bab8f
Mateusz Sikora <ms1qaz@gmail.com>
2025-12-25 15:25:26 +0100

    skip update when reporter is not in current validators

--- packages/jam/transition/statistics.ts
+++ packages/jam/transition/statistics.ts
@@ -243,6 +243,9 @@ export class Statistics {
 
     for (const reporter of input.reporters) {
       const index = validatorKeys.findIndex((x) => x.isEqualTo(reporter));
+      if (index === -1) {
+        continue; // Reporter not in current validator set
+      }
       const newGuaranteesCount = current[index].guarantees + 1;
       current[index].guarantees = tryAsU32(newGuaranteesCount);
     }
