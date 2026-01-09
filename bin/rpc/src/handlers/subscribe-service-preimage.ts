196fbb83014cd2314a758130b9d15f84bdb2b127
Sebastian Koszuta <sebastian.koszuta@gmail.com>
2026-01-09 11:09:28 +0100

    removed a leftover comment

--- bin/rpc/src/handlers/subscribe-service-preimage.ts
+++ bin/rpc/src/handlers/subscribe-service-preimage.ts
@@ -7,8 +7,6 @@ import { servicePreimage } from "./service-preimage.js";
  * https://github.com/polkadot-fellows/JIPs/blob/772ce90bfc33f4e1de9de3bbe10c561753cc0d41/JIP-2.md#subscribeservicepreimageid-hash-finalized
  */
 export const subscribeServicePreimage: Handler<"subscribeServicePreimage"> = async (params, { subscription }) => {
-  // return subscription.subscribe("subscribeServicePreimage",  params);
-
   return subscription.subscribe(
     "subscribeServicePreimage",
     async ([serviceId, preimageHash, finalized], context) => {
