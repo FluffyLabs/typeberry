ec38e0b2652b25c1f49adac1a30dc5d4f2735318
Tomek Drwięga <tomusdrw@users.noreply.github.com>
2026-04-14 20:01:56 +0100

    Authorization checking (#940)
    * IsAuthorized invocation
    
    * Is authorized tests
    
    * Split in-core into refine and is-authorized
    
    * add doc
    
    * Fix circular depedendecy
    
    * no exports on failure

--- packages/jam/transition/externalities/index.ts
+++ packages/jam/transition/externalities/index.ts
@@ -1,4 +1,5 @@
 export * from "./accumulate-externalities.js";
 export * from "./accumulate-fetch-externalities.js";
 export * from "./fetch-externalities.js";
+export * from "./is-authorized-fetch-externalities.js";
 export * from "./refine-fetch-externalities.js";
