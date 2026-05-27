f9ec276a7e5a584114b826f028bb0f9308a8be06
Tomasz Drwięga <tomusdrw@gmail.com>
2026-05-27 15:39:07 +0200

    Use 7G heap

--- bin/jam/build-for-npm.sh
+++ bin/jam/build-for-npm.sh
@@ -73,8 +73,8 @@ flatten_worker block-authorship bootstrap-generator
 # copy worker wasm files
 cp **/*.wasm ./ || true # ignore overwrite errors
 
-# Make index.js executable and insert shebang with 4GB heap size limit
-echo '#!/usr/bin/env -S node --max-old-space-size=4096' > ./temp.js && cat ./index.js >> ./temp.js && mv ./temp.js ./index.js
+# Make index.js executable and insert shebang with 7GB heap size limit
+echo '#!/usr/bin/env -S node --max-old-space-size=7168' > ./temp.js && cat ./index.js >> ./temp.js && mv ./temp.js ./index.js
 chmod +x ./index.js
 
 # build package.json file
