f6f992c029bd1e88ea555f1d49102a14c31a57de
Tomasz Drwięga <tomusdrw@gmail.com>
2026-05-27 14:25:30 +0200

    Lower heap and go back to ananas

--- bin/jam/build-for-npm.sh
+++ bin/jam/build-for-npm.sh
@@ -73,8 +73,8 @@ flatten_worker block-authorship bootstrap-generator
 # copy worker wasm files
 cp **/*.wasm ./ || true # ignore overwrite errors
 
-# Make index.js executable and insert shebang with 12GB heap size (leaves headroom on a 16GB box)
-echo '#!/usr/bin/env -S node --max-old-space-size=12288' > ./temp.js && cat ./index.js >> ./temp.js && mv ./temp.js ./index.js
+# Make index.js executable and insert shebang with 4GB heap size limit
+echo '#!/usr/bin/env -S node --max-old-space-size=4096' > ./temp.js && cat ./index.js >> ./temp.js && mv ./temp.js ./index.js
 chmod +x ./index.js
 
 # build package.json file
